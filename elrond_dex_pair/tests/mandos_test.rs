5f09844ec9294965cdffdb260a2f5d9f1fcf6501
catalinnnn <neagu.catalin.f@gmail.com>
2021-03-14 14:45:15 +0200

    Added mandos tests + small fixes after running tests

--- elrond_dex_pair/tests/mandos_test.rs
+++ elrond_dex_pair/tests/mandos_test.rs
@@ -6,13 +6,38 @@ use elrond_wasm_debug::*;
 fn contract_map() -> ContractMap<TxContext> {
 	let mut contract_map = ContractMap::new();
 	contract_map.register_contract(
-		"file:../output/adder.wasm",
-		Box::new(|context| Box::new(AdderImpl::new(context))),
+		"file:../output/elrond_dex_pair.wasm",
+		Box::new(|context| Box::new(PairImpl::new(context))),
 	);
 	contract_map
 }
 
 #[test]
-fn test_mandos() {
-	parse_execute_mandos("mandos/adder.scen.json", &contract_map());
+fn accept_esdt_payment_test() {
+	parse_execute_mandos("mandos/accept_esdt_payment.scen.json", &contract_map());
+}
+
+#[test]
+fn reclaim_temporary_funds_test() {
+	parse_execute_mandos("mandos/reclaim_temporary_funds.scen.json", &contract_map());
+}
+
+#[test]
+fn add_liquidity_test() {
+	parse_execute_mandos("mandos/add_liquidity.scen.json", &contract_map());
+}
+
+#[test]
+fn remove_liquidity_test() {
+	parse_execute_mandos("mandos/remove_liquidity.scen.json", &contract_map());
+}
+
+#[test]
+fn swap_fixed_input_test() {
+	parse_execute_mandos("mandos/swap_fixed_input.scen.json", &contract_map());
+}
+
+#[test]
+fn swap_fixed_output_test() {
+	parse_execute_mandos("mandos/swap_fixed_output.scen.json", &contract_map());
 }
