d5da5a2673b9ca304181a968746daf3278218379
catalinnnn <neagu.catalin.f@gmail.com>
2021-03-21 17:12:18 +0200

    [Staking SC]  Wip on stake, unstake, unbond operations. Added endpoints and calculus

--- elrond_dex_staking/src/lib.rs
+++ elrond_dex_staking/src/lib.rs
@@ -4,6 +4,19 @@ elrond_wasm::imports!();
 elrond_wasm::derive_imports!();
 
 
+
+pub mod liquidity_pool;
+pub use crate::liquidity_pool::*;
+
+// used as mock attributes for NFTs
+#[derive(TopEncode, TopDecode, TypeAbi)]
+pub struct SFTAttributes<BigUint: BigUintApi> {
+	lp_token_id: TokenIdentifier,
+	total_lp_tokens: BigUint,
+	total_initial_worth: BigUint,
+	total_amount_liquidity: BigUint
+}
+
 #[elrond_wasm_derive::callable(PairContractProxy)]
 pub trait PairContract {
 	fn get_tokens_for_given_position(&self, amount: BigUint) -> ContractCall<BigUint>;
@@ -11,31 +24,191 @@ pub trait PairContract {
 
 #[elrond_wasm_derive::contract(StakingImpl)]
 pub trait Staking {
+
+	#[module(LiquidityPoolModuleImpl)]
+	fn liquidity_pool(&self) -> LiquidityPoolModuleImpl<T, BigInt, BigUint>;
+
 	#[init]
 	fn init(&self, wegld_token_identifier: TokenIdentifier) {
 		self.wegld_token_identifier().set(&wegld_token_identifier);
+		self.liquidity_pool().virtual_token_id().set(&wegld_token_identifier);
 	}
 
 	#[payable("*")]
-	#[endpoint(stakeLpTokens)]
-	fn stake_lp_tokens(
+	#[endpoint(stake)]
+	fn stake(
 		&self,
-		#[payment_token] _lp_token: TokenIdentifier,
-		#[payment] _amount: BigUint,
-	) -> SCResult<AsyncCall<BigUint>> {
+		#[payment_token] lp_token: TokenIdentifier,
+		#[payment] amount: BigUint,
+	) -> SCResult<()> {
 
-		return sc_error!("Not yet implemented!");
+		let pair = self.get_pair_for_lp_token(&lp_token);
+		require!(pair != Address::zero(), "Unknown lp token");
+
+		//TODO: Ask get_tokens_for_given_position 	with execute on dest context
+		let wegld_amount = BigUint::zero();
+
+		let liquidity = sc_try!(self.liquidity_pool().add_liquidity(wegld_amount.clone()));
+
+		let attributes = SFTAttributes{
+			lp_token_id: lp_token.clone(),
+			total_lp_tokens: amount.clone(),
+			total_initial_worth: wegld_amount.clone(),
+			total_amount_liquidity: liquidity.clone()
+		};
+
+		//TODO: Create STF with amount $(liquidity) with $(attributes) and send
+		Ok(())
 	}
 
 	#[payable("*")]
-	#[endpoint(unstakeLpTokens)]
-	fn unstake_lp_tokens(
+	#[endpoint(unstake)]
+	fn unstake(
 		&self,
-		#[payment_token] _staking_token: TokenIdentifier,
-		#[payment] _amount: BigUint,
+		#[payment_token] staking_token: TokenIdentifier,
+		#[payment] liquidity: BigUint,
+	) -> SCResult<()> {
+
+		let sft_id = self.sft_staking_token_identifier().get();
+		require!(staking_token == sft_id, "Unknown staking token");
+
+		//TODO: Add actual read of attributes
+		let attributes = SFTAttributes{
+			lp_token_id: staking_token.clone(),
+			total_lp_tokens: liquidity.clone(),
+			total_initial_worth: liquidity.clone(),
+			total_amount_liquidity: liquidity.clone()
+		};
+
+		let pair = self.get_pair_for_lp_token(&attributes.lp_token_id);
+		require!(pair != Address::zero(), "Unknown lp token");
+
+		let initial_worth = attributes.total_initial_worth.clone() * liquidity.clone() / 
+			attributes.total_amount_liquidity.clone();
+		require!(initial_worth > 0, "Cannot unstake with intial_worth == 0");
+		let lp_tokens = attributes.total_lp_tokens.clone() * liquidity.clone() / 
+			attributes.total_amount_liquidity.clone();
+		require!(lp_tokens > 0, "Cannot unstake with lp_tokens == 0");
+
+		let reward = sc_try!(self.liquidity_pool().remove_liquidity(liquidity.clone(), initial_worth.clone()));
+		if reward != BigUint::zero() {
+			let wegld_balance = self.get_esdt_balance(
+				&self.get_sc_address(),
+				self.wegld_token_identifier().get().as_esdt_identifier(),
+				0,
+			);
+			//TODO: Add invariant. Something went really wrong.
+			require!(wegld_balance > reward, "Not enough funds");
+
+			self.send().direct_esdt_via_transf_exec(
+				&self.get_caller(),
+				self.wegld_token_identifier().get().as_esdt_identifier(),
+				&reward,
+				&[]
+			);
+		}
+
+		//Burn SFT $(liquidity) tokens with type $(staking_token) + add invariant + require!
+
+		let mut unstake_amount = self.get_unstake_amount(&self.get_caller(), &attributes.lp_token_id);
+		unstake_amount += lp_tokens;
+		self.set_unstake_amount(&self.get_caller(), &attributes.lp_token_id, &unstake_amount);
+		self.set_unbond_epoch(&self.get_caller(), &attributes.lp_token_id, self.get_block_epoch() + 14400); //10 days
+
+		Ok(())
+	}
+
+	#[endpoint(unbond)]
+	fn unbond(
+		&self,
+		lp_token: TokenIdentifier
+	) -> SCResult<()> {
+
+		let caller = self.get_caller();
+		require!(!self.is_empty_unstake_amount(&caller, &lp_token), "Don't have anything to unbond");
+		let block_epoch = self.get_block_epoch();
+		let unbond_epoch = self.get_unbond_epoch(&self.get_caller(), &lp_token);
+		require!(block_epoch >= unbond_epoch, "Unbond called too early");
+
+		let unstake_amount = self.get_unstake_amount(&self.get_caller(), &lp_token);
+		let lp_token_balance = self.get_esdt_balance(
+			&self.get_sc_address(),
+			lp_token.as_esdt_identifier(),
+			0,
+		);
+		//TODO: Add invariant. Something went really wrong.
+		require!(lp_token_balance > unstake_amount, "Not enough lp tokens");
+
+		self.send().direct_esdt_via_transf_exec(
+			&self.get_caller(),
+			lp_token.as_esdt_identifier(),
+			&unstake_amount,
+			&[]
+		);
+
+		self.clear_unstake_amount(&caller, &lp_token);
+		self.clear_unbond_epoch(&caller, &lp_token);
+		Ok(())
+	}
+
+	#[payable("EGLD")]
+	#[endpoint(sftIssue)]
+	fn sft_issue(
+		&self,
+		#[payment] issue_cost: BigUint,
+		token_display_name: BoxedBytes,
+		token_ticker: BoxedBytes,
 	) -> SCResult<AsyncCall<BigUint>> {
 
-		return sc_error!("Not yet implemented!");
+		only_owner!(self, "Permission denied");
+		if !self.sft_staking_token_identifier().is_empty() {
+			return sc_error!("Already issued");
+		}
+
+		let caller = self.get_caller();
+		Ok(ESDTSystemSmartContractProxy::new()
+			.issue_semi_fungible(
+				issue_cost,
+				&token_display_name,
+				&token_ticker,
+				true,
+				true,
+				true,
+				true,
+				true,
+				true,
+			)
+			.async_call()
+			.with_callback(self.callbacks().sft_issue_callback(&caller))
+		)
+	}
+
+	#[callback]
+	fn sft_issue_callback(
+		&self,
+		caller: &Address,
+		#[call_result] result: AsyncCallResult<TokenIdentifier>,
+	) {
+
+		let mut success = false;
+		match result {
+			AsyncCallResult::Ok(token_identifier) => {
+				if self.sft_staking_token_identifier().is_empty() {
+					success = true;
+					self.sft_staking_token_identifier().set(&token_identifier);
+				}
+			},
+			AsyncCallResult::Err(_) => {
+				success = false;
+			},
+		}
+
+		if success == false {
+			let (returned_tokens, token_identifier) = self.call_value().payment_token_pair();
+			if token_identifier.is_egld() && returned_tokens > 0 {
+				self.send().direct_egld(caller, &returned_tokens, &[]);
+			}
+		}
 	}
 
 	#[view(getPairForLpToken)]
@@ -61,12 +234,31 @@ pub trait Staking {
 	#[storage_mapper("wegld_token_identifier")]
 	fn wegld_token_identifier(&self) -> SingleValueMapper<Self::Storage, TokenIdentifier>;
 
-	#[view(getVirtualWeGLDReserve)]
-	#[storage_mapper("virtual_wegld_reserve")]
-	fn virtual_wegld_reserve(&self) -> SingleValueMapper<Self::Storage, BigUint>;
-
-
 	#[view(getSftStakingTokenIdentifier)]
 	#[storage_mapper("sft_staking_token_identifier")]
 	fn sft_staking_token_identifier(&self) -> SingleValueMapper<Self::Storage, TokenIdentifier>;
+
+
+	#[storage_get("unbond_epoch")]
+	fn get_unbond_epoch(&self, address: &Address, token: &TokenIdentifier) -> u64;
+
+	#[storage_set("unbond_epoch")]
+	fn set_unbond_epoch(&self, address: &Address, token: &TokenIdentifier, epoch: u64);
+
+	#[storage_clear("unbond_epoch")]
+	fn clear_unbond_epoch(&self, address: &Address, token: &TokenIdentifier);
+
+
+	#[storage_get("unstake_amount")]
+	fn get_unstake_amount(&self, address: &Address, token: &TokenIdentifier) -> BigUint;
+
+	#[storage_set("unstake_amount")]
+	fn set_unstake_amount(&self, address: &Address, token: &TokenIdentifier, amount: &BigUint);
+
+	#[storage_clear("unstake_amount")]
+	fn clear_unstake_amount(&self, address: &Address, token: &TokenIdentifier);
+	
+	#[storage_is_empty("unstake_amount")]
+	fn is_empty_unstake_amount(&self, address: &Address, token: &TokenIdentifier) -> bool;
 }
+
---
+++ elrond_dex_staking/src/liquidity_pool.rs
@@ -0,0 +1,87 @@
+imports!();
+derive_imports!();
+
+#[elrond_wasm_derive::module(LiquidityPoolModuleImpl)]
+pub trait LiquidityPoolModule {
+
+	fn add_liquidity(
+		&self,
+		amount: BigUint
+	) -> SCResult<BigUint> {
+		require!(amount > 0, "Amount needs to be greater than 0");
+
+		let mut total_supply = self.total_supply().get();
+		let mut virtual_reserves = self.virtual_reserves().get();
+		let actual_reserves = self.get_esdt_balance(
+			&self.get_sc_address(),
+			self.virtual_token_id().get().as_esdt_identifier(),
+			0,
+		);
+		let liquidity: BigUint;
+
+		if total_supply == 0 {
+			require!(amount > BigUint::from(1000u64), "First Stake needs to be greater than minimum amount: 1000 * 1000e-18");
+			liquidity = amount.clone() - BigUint::from(1000u64);
+		} else {
+			let total_reserves = virtual_reserves.clone() + actual_reserves.clone();
+			liquidity = amount.clone() * total_supply.clone() / total_reserves;
+		}
+		require!(liquidity > 0, "Insuficient liquidity minted");
+
+		virtual_reserves += amount;
+		self.virtual_reserves().set(&virtual_reserves);
+
+		total_supply += liquidity.clone();
+		self.total_supply().set(&total_supply);
+
+		Ok(liquidity)
+	}
+
+	fn remove_liquidity(
+		&self,
+		liquidity: BigUint,
+		initial_worth: BigUint
+	) -> SCResult<BigUint> {
+		require!(liquidity > 0, "Liquidity needs to be greater than 0");
+
+		let mut total_supply = self.total_supply().get();
+		require!(total_supply > liquidity, "Removing more liquidity than existent");
+
+		let mut virtual_reserves = self.virtual_reserves().get();
+		require!(virtual_reserves > initial_worth, "Removing more virtual reserve than existent");
+
+		let actual_reserves = self.get_esdt_balance(
+			&self.get_sc_address(),
+			self.virtual_token_id().get().as_esdt_identifier(),
+			0,
+		);
+		let reward: BigUint;
+
+		let total_reserves = virtual_reserves.clone() + actual_reserves.clone();
+		let worth = liquidity.clone() * total_reserves / total_supply.clone();
+
+		total_supply -= liquidity;
+		virtual_reserves -= initial_worth.clone();
+
+		if worth > initial_worth {
+			reward = worth - initial_worth;
+		}
+		else {
+			reward = BigUint::zero();
+		}
+
+		Ok(reward)
+	}
+
+	#[view(getTotalSupply)]
+	#[storage_mapper("total_supply")]
+	fn total_supply(&self) -> SingleValueMapper<Self::Storage, BigUint>;
+
+	#[view(getVirtualReserves)]
+	#[storage_mapper("virtual_reserves")]
+	fn virtual_reserves(&self) -> SingleValueMapper<Self::Storage, BigUint>;
+
+	#[storage_mapper("virtual_token_id")]
+	fn virtual_token_id(&self) -> SingleValueMapper<Self::Storage, TokenIdentifier>;
+}
+
