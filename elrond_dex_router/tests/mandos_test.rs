7c1d157077183f6626ce2b7a65bbebe7b2981291
Catalin Neagu <neagu.catalin.f@gmail.com>
2021-03-30 14:57:51 +0300

    [Router SC] Added mandos tests. Wip

--- elrond_dex_router/tests/mandos_test.rs
+++ elrond_dex_router/tests/mandos_test.rs
@@ -6,13 +6,33 @@ use elrond_wasm_debug::*;
 fn contract_map() -> ContractMap<TxContext> {
 	let mut contract_map = ContractMap::new();
 	contract_map.register_contract(
-		"file:../output/adder.wasm",
-		Box::new(|context| Box::new(AdderImpl::new(context))),
+		"file:../output/elrond_dex_router.wasm",
+		Box::new(|context| Box::new(RouterImpl::new(context))),
 	);
 	contract_map
 }
 
 #[test]
-fn test_mandos() {
-	parse_execute_mandos("mandos/adder.scen.json", &contract_map());
+fn construct_code_test() {
+	parse_execute_mandos("mandos/construct_code.scen.json", &contract_map());
+}
+
+#[test]
+fn construct_code_test() {
+	parse_execute_mandos("mandos/create_pair.scen.json", &contract_map());
+}
+
+#[test]
+fn construct_code_test() {
+	parse_execute_mandos("mandos/get_pair_views.scen.json", &contract_map());
+}
+
+#[test]
+fn construct_code_test() {
+	parse_execute_mandos("mandos/pause.scen.json", &contract_map());
+}
+
+#[test]
+fn construct_code_test() {
+	parse_execute_mandos("mandos/pause.scen.json", &contract_map());
 }
