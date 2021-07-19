52f45b6cfebe62b56a7dd1274614a3abb1c97b2d
Catalin Neagu <neagu.catalin.f@gmail.com>
2021-07-19 14:22:12 +0300

    removed validate functions

--- distribution/sc-proxy-dex/src/proxy_common.rs
+++ distribution/sc-proxy-dex/src/proxy_common.rs
@@ -4,7 +4,6 @@ elrond_wasm::derive_imports!();
 use common_structs::Nonce;
 use common_structs::{WrappedFarmTokenAttributes, WrappedLpTokenAttributes};
 
-const MAX_FUNDS_ENTRIES: usize = 10;
 pub const ACCEPT_PAY_FUNC_NAME: &[u8] = b"acceptPay";
 
 #[elrond_wasm_derive::module]
@@ -16,104 +15,7 @@ pub trait ProxyCommonModule {
 
     #[payable("*")]
     #[endpoint(acceptPay)]
-    fn accept_pay(
-        &self,
-        #[payment_token] token_id: TokenIdentifier,
-        #[payment_amount] amount: Self::BigUint,
-        #[payment_nonce] token_nonce: Nonce,
-    ) {
-        if amount == 0 {
-            return;
-        }
-
-        if self.current_tx_accepted_funds().len() > MAX_FUNDS_ENTRIES {
-            self.current_tx_accepted_funds().clear();
-        }
-
-        let entry = self
-            .current_tx_accepted_funds()
-            .get(&(token_id.clone(), token_nonce));
-        match entry {
-            Some(value) => {
-                self.current_tx_accepted_funds()
-                    .insert((token_id, token_nonce), value + amount);
-            }
-            None => {
-                self.current_tx_accepted_funds()
-                    .insert((token_id, token_nonce), amount);
-            }
-        }
-    }
-
-    fn reset_received_funds_on_current_tx(&self) {
-        self.current_tx_accepted_funds().clear();
-    }
-
-    fn validate_received_funds_chunk(
-        &self,
-        received_funds: Vec<(&TokenIdentifier, Nonce, &Self::BigUint)>,
-    ) -> SCResult<()> {
-        let big_zero = Self::BigUint::zero();
-
-        for funds in received_funds {
-            let token_id = funds.0;
-            let nonce = funds.1;
-            let amount = funds.2;
-
-            if amount == &big_zero {
-                continue;
-            }
-
-            self.validate_received_funds_on_current_tx(token_id, nonce, amount)?;
-            let old_amount = self
-                .current_tx_accepted_funds()
-                .get(&(token_id.clone(), nonce))
-                .unwrap();
-
-            if &old_amount == amount {
-                self.current_tx_accepted_funds()
-                    .remove(&(token_id.clone(), nonce));
-            } else {
-                self.current_tx_accepted_funds()
-                    .insert((token_id.clone(), nonce), &old_amount - amount);
-            }
-        }
-
-        require!(
-            self.current_tx_accepted_funds().is_empty(),
-            "More funds were received"
-        );
-
-        Ok(())
-    }
-
-    fn validate_received_funds_on_current_tx(
-        &self,
-        token_id: &TokenIdentifier,
-        token_nonce: Nonce,
-        amount: &Self::BigUint,
-    ) -> SCResult<()> {
-        if amount == &Self::BigUint::zero() {
-            return Ok(());
-        }
-
-        let result = self
-            .current_tx_accepted_funds()
-            .get(&(token_id.clone(), token_nonce));
-
-        match result {
-            Some(available_amount) => {
-                if &available_amount >= amount {
-                    Ok(())
-                } else {
-                    sc_error!("Available amount is not enough")
-                }
-            }
-            None => {
-                sc_error!("No available funds of this type")
-            }
-        }
-    }
+    fn accept_pay(&self) {}
 
     fn direct_generic(
         &self,
