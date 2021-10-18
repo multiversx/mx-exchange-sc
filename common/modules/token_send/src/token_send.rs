5fa8fd920fc03816a716dbd488b2154c5b6930a9
Catalin Neagu <neagu.catalin.f@gmail.com>
2021-10-18 15:57:12 +0300

    fix after review

--- common/modules/token_send/src/token_send.rs
+++ common/modules/token_send/src/token_send.rs
@@ -3,49 +3,17 @@
 elrond_wasm::imports!();
 elrond_wasm::derive_imports!();
 
-extern "C" {
-    fn transferESDTNFTExecute(
-        dstOffset: *const u8,
-        tokenIdOffset: *const u8,
-        tokenIdLen: i32,
-        valueOffset: *const u8,
-        nonce: i64,
-        gasLimit: i64,
-        functionOffset: *const u8,
-        functionLength: i32,
-        numArguments: i32,
-        argumentsLengthOffset: *const u8,
-        dataOffset: *const u8,
-    ) -> i32;
-    fn bigIntUnsignedByteLength(x: i32) -> i32;
-    fn bigIntGetUnsignedBytes(reference: i32, byte_ptr: *mut u8) -> i32;
-}
-
-pub type Handle = i32;
-const BUFFER_SIZE: usize = 32;
-static mut BUFFER_CUSTOM: [u8; BUFFER_SIZE] = [b'u'; BUFFER_SIZE];
-
-unsafe fn clear_buffer_custom() {
-    core::ptr::write_bytes(BUFFER_CUSTOM.as_mut_ptr(), 0u8, BUFFER_SIZE);
-}
-
-unsafe fn buffer_ptr_custom() -> *mut u8 {
-    BUFFER_CUSTOM.as_mut_ptr()
-}
-
-unsafe fn unsafe_buffer_load_be_pad_right_custom(bi_handle: Handle, nr_bytes: usize) -> *const u8 {
-    let byte_len = bigIntUnsignedByteLength(bi_handle) as usize;
-    if byte_len > nr_bytes {
-        panic!("critical err, bad len");
-    }
-    clear_buffer_custom();
-    if byte_len > 0 {
-        bigIntGetUnsignedBytes(bi_handle, buffer_ptr_custom().add(nr_bytes - byte_len));
-    }
-    buffer_ptr_custom()
-}
-
 use common_structs::Nonce;
+
+extern "C" {
+    fn managedMultiTransferESDTNFTExecute(
+        dstHandle: i32,
+        tokenTransfersHandle: i32,
+        gasLimit: i64,
+        functionHandle: i32,
+        argumentsHandle: i32,
+    ) -> i32;
+}
 
 #[elrond_wasm::module]
 pub trait TokenSendModule {
@@ -168,8 +136,6 @@ pub trait TokenSendModule {
         amount: &BigUint,
         opt_accept_funds_func: &OptionalArg<BoxedBytes>,
     ) -> SCResult<()> {
-        let to_address = to.to_address();
-        let arg_buffer = ManagedArgBuffer::new_empty(self.type_manager());
         let (function, gas_limit) = match opt_accept_funds_func {
             OptionalArg::Some(accept_funds_func) => (
                 accept_funds_func.as_slice(),
@@ -180,27 +146,24 @@ pub trait TokenSendModule {
                 (no_func, 0u64)
             }
         };
+        let arg_buffer = ManagedArgBuffer::new_empty(self.type_manager());
+        let endpoint_name = ManagedBuffer::new_from_bytes(self.type_manager(), function);
+        let mut payments = ManagedVec::new();
+        payments.push(EsdtTokenPayment::from(token.clone(), nonce, amount.clone()));
 
         unsafe {
-            let amount_bytes32_ptr =
-                unsafe_buffer_load_be_pad_right_custom(amount.get_raw_handle(), 32);
-            let function = BoxedBytes::from(function);
-            let legacy_arg_buffer = arg_buffer.to_legacy_arg_buffer();
-            let result = transferESDTNFTExecute(
-                to_address.as_ptr(),
-                token.to_esdt_identifier().as_ptr(),
-                token.len() as i32,
-                amount_bytes32_ptr,
-                nonce as i64,
+            let result = managedMultiTransferESDTNFTExecute(
+                to.get_raw_handle(),
+                payments.get_raw_handle(),
                 gas_limit as i64,
-                function.as_ptr(),
-                function.len() as i32,
-                legacy_arg_buffer.num_args() as i32,
-                legacy_arg_buffer.arg_lengths_bytes_ptr(),
-                legacy_arg_buffer.arg_data_ptr(),
+                endpoint_name.get_raw_handle(),
+                arg_buffer.get_raw_handle(),
             );
-            require!(result == 0, "bad result");
-            Ok(())
+            if result == 0 {
+                Ok(()).into()
+            } else {
+                core::result::Result::Err(&b"multiTransferESDTNFTExecute failed"[..]).into()
+            }
         }
     }
 
