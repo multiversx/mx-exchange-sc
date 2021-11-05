2a3a30fce2ab52233375e7d76c1a87c63e2c7125
Catalin Neagu <neagu.catalin.f@gmail.com>
2021-11-05 20:19:26 +0200

    moved previous check in another function

--- locked-asset/proxy-dex/src/wrapped_farm_token_merge.rs
+++ locked-asset/proxy-dex/src/wrapped_farm_token_merge.rs
@@ -70,12 +70,10 @@ pub trait WrappedFarmTokenMerge:
         self.require_all_tokens_are_wrapped_farm_tokens(payments, &wrapped_farm_token_id)?;
 
         let mut tokens = self.get_wrapped_farm_tokens_from_deposit(payments);
-        self.require_all_wrapped_farm_tokens_have_same_farming_token(&tokens, replic.clone())?;
-
         if replic.is_some() {
             tokens.push(replic.unwrap());
         }
-        self.require_wrapped_farm_tokens_from_same_farm(&tokens)?;
+        self.require_wrapped_farm_tokens_have_same_token_ids(&tokens)?;
 
         let merged_farm_token_amount = self.merge_farm_tokens(farm_contract, &tokens);
         let farming_token_amount = self.merge_farming_tokens(&tokens)?;
@@ -130,16 +128,21 @@ pub trait WrappedFarmTokenMerge:
         result
     }
 
-    fn require_wrapped_farm_tokens_from_same_farm(
+    fn require_wrapped_farm_tokens_have_same_token_ids(
         &self,
         tokens: &[WrappedFarmToken<Self::Api>],
     ) -> SCResult<()> {
         let farm_token_id = tokens[0].attributes.farm_token_id.clone();
+        let farming_token_id = tokens[0].attributes.farming_token_id.clone();
 
         for elem in tokens.iter() {
             require!(
                 elem.attributes.farm_token_id == farm_token_id,
                 "Farm token id differs"
+            );
+            require!(
+                elem.attributes.farming_token_id == farming_token_id,
+                "Farming token id differs"
             );
         }
         Ok(())
@@ -155,32 +158,6 @@ pub trait WrappedFarmTokenMerge:
                 &elem.token_identifier == wrapped_farm_token_id,
                 "Not a Wrapped Farm Token"
             );
-        }
-        Ok(())
-    }
-
-    fn require_all_wrapped_farm_tokens_have_same_farming_token(
-        &self,
-        wrapped_farm_tokens: &[WrappedFarmToken<Self::Api>],
-        replic: Option<WrappedFarmToken<Self::Api>>,
-    ) -> SCResult<()> {
-        if !wrapped_farm_tokens.is_empty() {
-            let first_wrapped_farm_token = wrapped_farm_tokens.first().unwrap();
-
-            for wrapped_farm_token in wrapped_farm_tokens.iter() {
-                require!(
-                    wrapped_farm_token.attributes.farming_token_id == first_wrapped_farm_token.attributes.farming_token_id,
-                    "bad token input"
-                );
-            }
-
-            if replic.is_some() {
-                let replic_unwrap = replic.unwrap();
-
-                require!(replic_unwrap.attributes.farming_token_id == first_wrapped_farm_token.attributes.farming_token_id,
-                    "bad token input"
-                );
-            }
         }
         Ok(())
     }
