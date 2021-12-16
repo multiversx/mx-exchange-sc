5c4d63189bea7d1d5ddb7d022ba30d27657e2ff0
Catalin Neagu <neagu.catalin.f@gmail.com>
2021-12-16 13:40:30 +0200

    better spacing

--- dex/pair/src/macros.rs
+++ dex/pair/src/macros.rs
@@ -3,20 +3,20 @@ elrond_wasm::derive_imports!();
 
 #[macro_export]
 macro_rules! err {
-    ($self:expr, $msg: expr,) => {
+    ($self:expr, $msg:expr,) => {
         err!($self, $cond, $msg)
     };
-    ($self:expr, $msg: expr) => {
+    ($self:expr, $msg:expr) => {
         $self.raw_vm_api().signal_error($msg)
     };
 }
 
 #[macro_export]
 macro_rules! kill {
-    ($self:expr, $cond: expr, $msg: expr,) => {
+    ($self:expr, $cond:expr, $msg:expr,) => {
         kill!($self, $cond, $msg)
     };
-    ($self:expr, $cond: expr, $msg: expr) => {
+    ($self:expr, $cond:expr, $msg:expr) => {
         if !$cond {
             crate::err!($self, $msg)
         }
