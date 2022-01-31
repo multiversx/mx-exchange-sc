1faf622e1a56a682e1eaa838328337668e8cc27a
dorin.iancu <dorin.iancu@elrond.com>
2022-01-31 15:27:51 +0200

    test setup

--- dex/farm-staking-proxy/tests/staking_farm_with_lp.rs
+++ dex/farm-staking-proxy/tests/staking_farm_with_lp.rs
@@ -1,4 +1,8 @@
-use elrond_wasm_debug::{rust_biguint, testing_framework::BlockchainStateWrapper};
+use elrond_wasm_debug::{
+    rust_biguint,
+    testing_framework::{BlockchainStateWrapper, ContractObjWrapper},
+    DebugApi,
+};
 
 pub mod constants;
 pub mod staking_farm_with_lp_external_contracts;
@@ -8,29 +12,81 @@ use constants::*;
 use staking_farm_with_lp_external_contracts::*;
 use staking_farm_with_lp_staking_contract_interactions::*;
 
-#[test]
-fn test_all_setup() {
-    let rust_zero = rust_biguint!(0u64);
-    let mut wrapper = BlockchainStateWrapper::new();
-    let owner_addr = wrapper.create_user_account(&rust_zero);
-    let user_addr = wrapper.create_user_account(&rust_biguint!(100_000_000));
+struct FarmStakingSetup<
+    PairObjBuilder,
+    FarmObjBuilder,
+    StakingContractObjBuilder,
+    ProxyContractObjBuilder,
+> where
+    PairObjBuilder: 'static + Copy + Fn() -> pair::ContractObj<DebugApi>,
+    FarmObjBuilder: 'static + Copy + Fn() -> farm::ContractObj<DebugApi>,
+    StakingContractObjBuilder: 'static + Copy + Fn() -> farm_staking::ContractObj<DebugApi>,
+    ProxyContractObjBuilder: 'static + Copy + Fn() -> farm_staking_proxy::ContractObj<DebugApi>,
+{
+    pub b_mock: BlockchainStateWrapper,
+    pub pair_wrapper: ContractObjWrapper<pair::ContractObj<DebugApi>, PairObjBuilder>,
+    pub lp_farm_wrapper: ContractObjWrapper<farm::ContractObj<DebugApi>, FarmObjBuilder>,
+    pub staking_farm_wrapper:
+        ContractObjWrapper<farm_staking::ContractObj<DebugApi>, StakingContractObjBuilder>,
+    pub proxy_wrapper:
+        ContractObjWrapper<farm_staking_proxy::ContractObj<DebugApi>, ProxyContractObjBuilder>,
+}
 
-    let pair_wrapper = setup_pair(&owner_addr, &user_addr, &mut wrapper, pair::contract_obj);
+fn setup_all<PairObjBuilder, FarmObjBuilder, StakingContractObjBuilder, ProxyContractObjBuilder>(
+    pair_builder: PairObjBuilder,
+    lp_farm_builder: FarmObjBuilder,
+    staking_farm_builder: StakingContractObjBuilder,
+    proxy_builder: ProxyContractObjBuilder,
+) -> FarmStakingSetup<
+    PairObjBuilder,
+    FarmObjBuilder,
+    StakingContractObjBuilder,
+    ProxyContractObjBuilder,
+>
+where
+    PairObjBuilder: 'static + Copy + Fn() -> pair::ContractObj<DebugApi>,
+    FarmObjBuilder: 'static + Copy + Fn() -> farm::ContractObj<DebugApi>,
+    StakingContractObjBuilder: 'static + Copy + Fn() -> farm_staking::ContractObj<DebugApi>,
+    ProxyContractObjBuilder: 'static + Copy + Fn() -> farm_staking_proxy::ContractObj<DebugApi>,
+{
+    let rust_zero = rust_biguint!(0u64);
+    let mut b_mock = BlockchainStateWrapper::new();
+    let owner_addr = b_mock.create_user_account(&rust_zero);
+    let user_addr = b_mock.create_user_account(&rust_biguint!(100_000_000));
+
+    let pair_wrapper = setup_pair(&owner_addr, &user_addr, &mut b_mock, pair_builder);
     let lp_farm_wrapper = setup_lp_farm(
         &owner_addr,
         &user_addr,
-        &mut wrapper,
-        farm::contract_obj,
+        &mut b_mock,
+        lp_farm_builder,
         USER_TOTAL_LP_TOKENS,
     );
-    let staking_farm_wrapper =
-        setup_staking_farm(&owner_addr, &mut wrapper, farm_staking::contract_obj);
-    let _proxy_wrapper = setup_proxy(
+    let staking_farm_wrapper = setup_staking_farm(&owner_addr, &mut b_mock, staking_farm_builder);
+    let proxy_wrapper = setup_proxy(
         &owner_addr,
         lp_farm_wrapper.address_ref(),
         staking_farm_wrapper.address_ref(),
         pair_wrapper.address_ref(),
-        &mut wrapper,
+        &mut b_mock,
+        proxy_builder,
+    );
+
+    FarmStakingSetup {
+        b_mock,
+        pair_wrapper,
+        lp_farm_wrapper,
+        staking_farm_wrapper,
+        proxy_wrapper,
+    }
+}
+
+#[test]
+fn test_all_setup() {
+    let _ = setup_all(
+        pair::contract_obj,
+        farm::contract_obj,
+        farm_staking::contract_obj,
         farm_staking_proxy::contract_obj,
     );
 }
