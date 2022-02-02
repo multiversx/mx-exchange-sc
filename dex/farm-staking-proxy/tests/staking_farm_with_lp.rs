24abff6b073603673246ceebdac81145c15667c2
dorin.iancu <dorin.iancu@elrond.com>
2022-02-02 15:58:52 +0200

    claim rewards half test

--- dex/farm-staking-proxy/tests/staking_farm_with_lp.rs
+++ dex/farm-staking-proxy/tests/staking_farm_with_lp.rs
@@ -56,3 +56,34 @@ fn test_claim_rewards_farm_proxy_full() {
         dual_yield_token_amount,
     );
 }
+
+#[test]
+fn test_claim_rewards_farm_proxy_half() {
+    let mut setup = FarmStakingSetup::new(
+        pair::contract_obj,
+        farm::contract_obj,
+        farm_staking::contract_obj,
+        farm_staking_proxy::contract_obj,
+    );
+
+    let expected_staking_token_amount = 1_001_000_000 / 2;
+    let dual_yield_token_nonce_after_stake = setup.stake_farm_lp(
+        1,
+        USER_TOTAL_LP_TOKENS / 2,
+        1,
+        expected_staking_token_amount,
+    );
+
+    setup
+        .b_mock
+        .set_block_nonce(BLOCK_NONCE_AFTER_PAIR_SETUP + 20);
+
+    let dual_yield_token_amount = expected_staking_token_amount;
+    let _dual_yield_token_nonce_after_claim = setup.claim_rewards(
+        dual_yield_token_nonce_after_stake,
+        dual_yield_token_amount,
+        99_999 / 2,
+        940, // ~= 1_900 / 2, approximations somewhere make it go slightly lower
+        dual_yield_token_amount,
+    );
+}
