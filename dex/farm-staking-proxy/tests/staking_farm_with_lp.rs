85d1a12d90e3ea3e1415e2c660af1c176a7aafd4
dorin.iancu <dorin.iancu@elrond.com>
2022-02-02 16:47:33 +0200

    unstake after claim test

--- dex/farm-staking-proxy/tests/staking_farm_with_lp.rs
+++ dex/farm-staking-proxy/tests/staking_farm_with_lp.rs
@@ -158,3 +158,41 @@ fn test_unstake_through_proxy_no_claim() {
         10,
     );
 }
+
+#[test]
+fn unstake_through_proxy_after_claim() {
+    let mut setup = FarmStakingSetup::new(
+        pair::contract_obj,
+        farm::contract_obj,
+        farm_staking::contract_obj,
+        farm_staking_proxy::contract_obj,
+    );
+
+    let expected_staking_token_amount = 1_001_000_000;
+    let dual_yield_token_nonce_after_stake =
+        setup.stake_farm_lp(1, USER_TOTAL_LP_TOKENS, 1, expected_staking_token_amount);
+
+    setup
+        .b_mock
+        .set_block_nonce(BLOCK_NONCE_AFTER_PAIR_SETUP + 20);
+
+    let dual_yield_token_amount = expected_staking_token_amount;
+    let dual_yield_token_nonce_after_claim = setup.claim_rewards(
+        dual_yield_token_nonce_after_stake,
+        dual_yield_token_amount,
+        99_999,
+        1_900,
+        dual_yield_token_amount,
+    );
+
+    let dual_yield_token_amount = 1_001_000_000;
+    setup.unstake(
+        dual_yield_token_nonce_after_claim,
+        dual_yield_token_amount,
+        999_999_000,
+        0,
+        0,
+        999_999_000,
+        10,
+    );
+}
