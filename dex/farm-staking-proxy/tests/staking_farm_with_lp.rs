d04abf9fe297135d2f583a6b5b2f9dfe84685a90
dorin.iancu <dorin.iancu@elrond.com>
2022-02-02 16:01:27 +0200

    claim twice test

--- dex/farm-staking-proxy/tests/staking_farm_with_lp.rs
+++ dex/farm-staking-proxy/tests/staking_farm_with_lp.rs
@@ -83,7 +83,49 @@ fn test_claim_rewards_farm_proxy_half() {
         dual_yield_token_nonce_after_stake,
         dual_yield_token_amount,
         99_999 / 2,
-        940, // ~= 1_900 / 2, approximations somewhere make it go slightly lower
+        940, // ~= 1_900 / 2 = 950, approximations somewhere make it go slightly lower
+        dual_yield_token_amount,
+    );
+}
+
+#[test]
+fn test_claim_rewards_farm_proxy_twice() {
+    let mut setup = FarmStakingSetup::new(
+        pair::contract_obj,
+        farm::contract_obj,
+        farm_staking::contract_obj,
+        farm_staking_proxy::contract_obj,
+    );
+
+    let expected_staking_token_amount = 1_001_000_000;
+    let dual_yield_token_nonce_after_stake =
+        setup.stake_farm_lp(1, USER_TOTAL_LP_TOKENS, 1, expected_staking_token_amount);
+
+    // first claim, at block 120
+    setup
+        .b_mock
+        .set_block_nonce(BLOCK_NONCE_AFTER_PAIR_SETUP + 20);
+
+    let dual_yield_token_amount = expected_staking_token_amount;
+    let dual_yield_token_nonce_after_first_claim = setup.claim_rewards(
+        dual_yield_token_nonce_after_stake,
+        dual_yield_token_amount,
+        99_999,
+        1_900,
+        dual_yield_token_amount,
+    );
+
+    // second claim, at block 140
+    setup
+        .b_mock
+        .set_block_nonce(BLOCK_NONCE_AFTER_PAIR_SETUP + 40);
+
+    let dual_yield_token_amount = expected_staking_token_amount;
+    let _ = setup.claim_rewards(
+        dual_yield_token_nonce_after_first_claim,
+        dual_yield_token_amount,
+        99_999,
+        1_900,
         dual_yield_token_amount,
     );
 }
