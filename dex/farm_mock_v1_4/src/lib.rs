5296e7abf94318f210d135d1817012fbeb83e5f6
Catalin Neagu <neagu.catalin.f@gmail.com>
2022-02-07 11:30:42 +0200

    added unit test for migration

---
+++ dex/farm_mock_v1_4/src/lib.rs
@@ -0,0 +1,19 @@
+#![no_std]
+
+elrond_wasm::imports!();
+elrond_wasm::derive_imports!();
+
+#[elrond_wasm::contract]
+pub trait FarmMockV14 {
+    #[init]
+    fn init(&self) {}
+
+    #[payable("*")]
+    #[endpoint(migrateFromV1_2Farm)]
+    fn migrate_from_v1_2_farm(&self, _orig_caller: ManagedAddress) -> EsdtTokenPayment<Self::Api> {
+        EsdtTokenPayment::new(TokenIdentifier::egld(), 0, BigUint::zero())
+    }
+
+    #[endpoint(setRpsAndStartRewards)]
+    fn set_rps_and_start_rewards(&self, _rps: BigUint) {}
+}
