4000a6e1c173748045732f2a02a65dbbc3962c59
dorin.iancu <dorin.iancu@elrond.com>
2022-03-07 15:30:46 +0200

    initial impl

---
+++ dex/farm-staking-contracts/metabonding-staking/wasm/src/lib.rs
@@ -0,0 +1,20 @@
+////////////////////////////////////////////////////
+////////////////// AUTO-GENERATED //////////////////
+////////////////////////////////////////////////////
+
+#![no_std]
+
+elrond_wasm_node::wasm_endpoints! {
+    metabonding_staking
+    (
+        getLockedAssetFactoryAddress
+        getLockedAssetTokenId
+        getSnapshot
+        getUserList
+        stakeLockedAsset
+        unbond
+        unstake
+    )
+}
+
+elrond_wasm_node::wasm_empty_callback! {}
