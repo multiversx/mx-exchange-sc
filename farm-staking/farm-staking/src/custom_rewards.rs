6712699397b8235eac75f2e94be955e6554ccd40
dorin.iancu <dorin.iancu@elrond.com>
2022-04-20 11:56:13 +0300

    dummy commit

--- farm-staking/farm-staking/src/custom_rewards.rs
+++ farm-staking/farm-staking/src/custom_rewards.rs
@@ -35,6 +35,7 @@ pub trait CustomRewardsModule:
 
     fn generate_aggregated_rewards(&self) {
         let mut extra_rewards = self.calculate_extra_rewards_since_last_allocation();
+
         if extra_rewards > 0 {
             let mut accumulated_rewards = self.accumulated_rewards().get();
             let total_rewards = &accumulated_rewards + &extra_rewards;
