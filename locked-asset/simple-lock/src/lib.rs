f9f9a003259d8a5c641cc3d9a56006e7b21e956b
dorin.iancu <dorin.iancu@elrond.com>
2022-09-13 16:06:07 +0300

    common file for basic lock unlock

--- locked-asset/simple-lock/src/lib.rs
+++ locked-asset/simple-lock/src/lib.rs
@@ -2,6 +2,7 @@
 
 elrond_wasm::imports!();
 
+pub mod basic_lock_unlock;
 pub mod error_messages;
 pub mod farm_interactions;
 pub mod locked_token;
@@ -10,12 +11,10 @@ pub mod proxy_farm;
 pub mod proxy_lp;
 pub mod token_attributes;
 
-use crate::locked_token::LockedTokenAttributes;
-use error_messages::*;
-
 #[elrond_wasm::contract]
 pub trait SimpleLock:
-    locked_token::LockedTokenModule
+    basic_lock_unlock::BasicLockUnlock
+    + locked_token::LockedTokenModule
     + elrond_wasm_modules::default_issue_callbacks::DefaultIssueCallbacksModule
     + proxy_lp::ProxyLpModule
     + proxy_farm::ProxyFarmModule
@@ -38,41 +37,15 @@ pub trait SimpleLock:
     /// Output payments: LOCKED tokens (or original payment if current_epoch >= unlock_epoch)
     #[payable("*")]
     #[endpoint(lockTokens)]
-    fn lock_tokens(
+    fn lock_tokens_endpoint(
         &self,
         unlock_epoch: u64,
         opt_destination: OptionalValue<ManagedAddress>,
     ) -> EgldOrEsdtTokenPayment<Self::Api> {
         let payment = self.call_value().egld_or_single_esdt();
-        require!(payment.amount > 0, NO_PAYMENT_ERR_MSG);
-
         let dest_address = self.dest_from_optional(opt_destination);
-        let current_epoch = self.blockchain().get_block_epoch();
-        if current_epoch >= unlock_epoch {
-            self.send().direct(
-                &dest_address,
-                &payment.token_identifier,
-                payment.token_nonce,
-                &payment.amount,
-            );
 
-            return payment;
-        }
-
-        let attributes = LockedTokenAttributes {
-            original_token_id: payment.token_identifier.clone(),
-            original_token_nonce: payment.token_nonce,
-            unlock_epoch,
-        };
-        let locked_token_mapper = self.locked_token();
-        let sft_nonce = self.get_or_create_nonce_for_attributes(
-            &locked_token_mapper,
-            &payment.token_identifier.into_name(),
-            &attributes,
-        );
-        self.locked_token()
-            .nft_add_quantity_and_send(&dest_address, sft_nonce, payment.amount)
-            .into()
+        self.lock_and_send(&dest_address, payment, unlock_epoch)
     }
 
     /// Unlock tokens, previously locked with the `lockTokens` endpoint
@@ -85,39 +58,14 @@ pub trait SimpleLock:
     /// Output payments: the originally locked tokens
     #[payable("*")]
     #[endpoint(unlockTokens)]
-    fn unlock_tokens(
+    fn unlock_tokens_endpoint(
         &self,
         opt_destination: OptionalValue<ManagedAddress>,
     ) -> EgldOrEsdtTokenPayment<Self::Api> {
-        let payment: EsdtTokenPayment<Self::Api> = self.call_value().single_esdt();
-        require!(payment.amount > 0, NO_PAYMENT_ERR_MSG);
-
-        let locked_token_mapper = self.locked_token();
-        locked_token_mapper.require_same_token(&payment.token_identifier);
-
-        let attributes: LockedTokenAttributes<Self::Api> =
-            locked_token_mapper.get_token_attributes(payment.token_nonce);
-        let current_epoch = self.blockchain().get_block_epoch();
-        require!(
-            current_epoch >= attributes.unlock_epoch,
-            "Cannot unlock yet"
-        );
-
-        locked_token_mapper.nft_burn(payment.token_nonce, &payment.amount);
-
+        let payment = self.call_value().single_esdt();
         let dest_address = self.dest_from_optional(opt_destination);
-        self.send().direct(
-            &dest_address,
-            &attributes.original_token_id,
-            attributes.original_token_nonce,
-            &payment.amount,
-        );
 
-        EgldOrEsdtTokenPayment::new(
-            attributes.original_token_id,
-            attributes.original_token_nonce,
-            payment.amount,
-        )
+        self.unlock_and_send(&dest_address, payment)
     }
 
     fn dest_from_optional(&self, opt_destination: OptionalValue<ManagedAddress>) -> ManagedAddress {
