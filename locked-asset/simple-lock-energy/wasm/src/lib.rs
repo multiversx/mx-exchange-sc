29b30279a7ff089ed04134b13dfc22788741dd73
dorin.iancu <dorin.iancu@elrond.com>
2022-09-14 13:19:21 +0300

    build

--- locked-asset/simple-lock-energy/wasm/src/lib.rs
+++ locked-asset/simple-lock-energy/wasm/src/lib.rs
@@ -8,10 +8,15 @@ elrond_wasm_node::wasm_endpoints! {
     simple_lock_energy
     (
         callBack
+        addLockOptions
         getBaseAssetTokenId
+        getEnergyAmountForUser
+        getEnergyEntryForUser
+        getLockOptions
         getLockedTokenId
         issueLockedToken
         lockTokens
+        removeLockOptions
         setLockedTokenId
         unlockTokens
     )
