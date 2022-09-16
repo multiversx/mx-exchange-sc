0101037930b3dc4a61c9f54725e55745f4815372
dorin.iancu <dorin.iancu@elrond.com>
2022-09-16 12:50:07 +0300

    invalid lock tests

--- locked-asset/simple-lock-energy/tests/simple_lock_energy_setup/mod.rs
+++ locked-asset/simple-lock-energy/tests/simple_lock_energy_setup/mod.rs
@@ -1,19 +1,23 @@
 use elrond_wasm::{
+    elrond_codec::multi_types::OptionalValue,
     storage::mappers::StorageTokenWrapper,
     types::{Address, EsdtLocalRole, MultiValueEncoded},
 };
 use elrond_wasm_debug::{
     managed_address, managed_token_id, rust_biguint,
     testing_framework::{BlockchainStateWrapper, ContractObjWrapper},
+    tx_mock::TxResult,
     DebugApi,
 };
+use elrond_wasm_modules::pause::PauseModule;
 use simple_lock::locked_token::LockedTokenModule;
-use simple_lock_energy::SimpleLockEnergy;
+use simple_lock_energy::{lock_options::LockOptionsModule, SimpleLockEnergy};
 
 mod fees_collector_mock;
 use fees_collector_mock::*;
 
 pub const EPOCHS_IN_YEAR: u64 = 365;
+pub const USER_BALANCE: u64 = 1_000_000_000_000_000_000;
 
 pub static BASE_ASSET_TOKEN_ID: &[u8] = b"MEX-123456";
 pub static LOCKED_TOKEN_ID: &[u8] = b"LOCKED-123456";
@@ -40,6 +44,7 @@ where
     ScBuilder: 'static + Copy + Fn() -> simple_lock_energy::ContractObj<DebugApi>,
 {
     pub fn new(sc_builder: ScBuilder) -> Self {
+        let _ = DebugApi::dummy();
         let rust_zero = rust_biguint!(0u64);
         let mut b_mock = BlockchainStateWrapper::new();
         let owner = b_mock.create_user_account(&rust_zero);
@@ -70,8 +75,11 @@ where
                     lock_options,
                 );
 
+                assert_eq!(sc.max_lock_option().get(), *LOCK_OPTIONS.last().unwrap());
+
                 sc.locked_token()
                     .set_token_id(&managed_token_id!(LOCKED_TOKEN_ID));
+                sc.set_paused(false);
             })
             .assert_ok();
 
@@ -86,6 +94,17 @@ where
             ],
         );
 
+        b_mock.set_esdt_balance(
+            &first_user,
+            BASE_ASSET_TOKEN_ID,
+            &rust_biguint!(USER_BALANCE),
+        );
+        b_mock.set_esdt_balance(
+            &second_user,
+            BASE_ASSET_TOKEN_ID,
+            &rust_biguint!(USER_BALANCE),
+        );
+
         Self {
             b_mock,
             owner,
@@ -94,5 +113,29 @@ where
             sc_wrapper,
             fees_collector_mock: fees_collector_mock.address_ref().clone(),
         }
+    }
+}
+
+impl<ScBuilder> SimpleLockEnergySetup<ScBuilder>
+where
+    ScBuilder: 'static + Copy + Fn() -> simple_lock_energy::ContractObj<DebugApi>,
+{
+    pub fn lock(
+        &mut self,
+        caller: &Address,
+        token_id: &[u8],
+        amount: u64,
+        lock_epochs: u64,
+    ) -> TxResult {
+        self.b_mock.execute_esdt_transfer(
+            caller,
+            &self.sc_wrapper,
+            token_id,
+            0,
+            &rust_biguint!(amount),
+            |sc| {
+                sc.lock_tokens_endpoint(lock_epochs, OptionalValue::Some(managed_address!(caller)));
+            },
+        )
     }
 }
--- locked-asset/simple-lock-energy/tests/simple_lock_energy_test.rs
+++ locked-asset/simple-lock-energy/tests/simple_lock_energy_test.rs
@@ -1,7 +1,29 @@
 mod simple_lock_energy_setup;
+
 use simple_lock_energy_setup::*;
+
+use elrond_wasm_debug::rust_biguint;
 
 #[test]
 fn init_test() {
     let _ = SimpleLockEnergySetup::new(simple_lock_energy::contract_obj);
+}
+
+#[test]
+fn try_lock() {
+    let mut setup = SimpleLockEnergySetup::new(simple_lock_energy::contract_obj);
+    let first_user = setup.first_user.clone();
+    setup
+        .b_mock
+        .set_esdt_balance(&first_user, b"FAKETOKEN-123456", &rust_biguint!(1_000));
+
+    // wrong token
+    setup
+        .lock(&first_user, b"FAKETOKEN-123456", 1_000, LOCK_OPTIONS[0])
+        .assert_user_error("May only lock the whitelisted token");
+
+    // invalid lock option
+    setup
+        .lock(&first_user, BASE_ASSET_TOKEN_ID, USER_BALANCE, 42)
+        .assert_user_error("Invalid lock choice");
 }
