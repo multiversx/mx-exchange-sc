a2654050b561489f224e3389b431f9db4c9cf915
dorin.iancu <dorin.iancu@elrond.com>
2022-09-27 12:21:00 +0300

    build

--- locked-asset/simple-lock-energy/wasm/src/lib.rs
+++ locked-asset/simple-lock-energy/wasm/src/lib.rs
@@ -9,39 +9,24 @@ elrond_wasm_node::wasm_endpoints! {
     (
         callBack
         acceptMigratedTokens
-        addFarmToWhitelist
-        addLiquidityLockedToken
         addLockOptions
-        addLpToWhitelist
-        enterFarmLockedToken
-        exitFarmLockedToken
         extendLockingPeriod
-        farmClaimRewardsLockedToken
         getBaseAssetTokenId
         getEnergyAmountForUser
         getEnergyEntryForUser
-        getFarmProxyTokenId
         getFeesBurnPercentage
         getFeesCollectorAddress
-        getKnownFarms
-        getKnownLiquidityPools
         getLockOptions
         getLockedTokenId
-        getLpProxyTokenId
         getOldTokenNonces
         getPenaltyAmount
         getPenaltyPercentage
         isPaused
-        issueFarmProxyToken
         issueLockedToken
-        issueLpProxyToken
         lockTokens
         pause
         reduceLockPeriod
-        removeFarmFromWhitelist
-        removeLiquidityLockedToken
         removeLockOptions
-        removeLpFromWhitelist
         setFeesBurnPercentage
         setFeesCollectorAddress
         setOldLockedAssetFactoryAddress
