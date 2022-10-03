45e60b417b4d9648c0fb729d446cc2f9faa8549b
dorin-iancu <72252689+dorin-iancu@users.noreply.github.com>
2022-10-03 13:42:03 +0300

    Update common/traits/mergeable/src/lib.rs

--- common/traits/mergeable/src/lib.rs
+++ common/traits/mergeable/src/lib.rs
@@ -6,7 +6,7 @@ pub static CANNOT_MERGE_ERR_MSG: &[u8] = b"Cannot merge";
 
 pub trait Mergeable<M: ManagedTypeApi> {
     fn error_if_not_mergeable(&self, other: &Self) {
-        if !self.can_merge_with(&other) {
+        if !self.can_merge_with(other) {
             M::error_api_impl().signal_error(CANNOT_MERGE_ERR_MSG);
         }
     }
