700c6501fb494da981f927a4a45aefa93c79fd8a
Claudiu Lataretu <claudiu.lataretu@gmail.com>
2022-10-17 17:16:24 +0300

    proxy_dex: fix token identifier type on set
    Signed-off-by: Claudiu Lataretu <claudiu.lataretu@gmail.com>

--- locked-asset/proxy_dex/src/lib.rs
+++ locked-asset/proxy_dex/src/lib.rs
@@ -65,7 +65,7 @@ pub trait ProxyDexImpl:
             require!(is_new, "Locked token already assigned to another address");
         }
 
-        self.asset_token().set_token_id(&asset_token_id);
+        self.asset_token().set_token_id(asset_token_id);
     }
 
     #[only_owner]
