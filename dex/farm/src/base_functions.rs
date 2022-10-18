c0bc92dd05c6c1093e03167d0589099283378c78
dorin.iancu <dorin.iancu@elrond.com>
2022-10-18 11:53:14 +0300

    no ongoing operation

--- dex/farm/src/base_functions.rs
+++ dex/farm/src/base_functions.rs
@@ -40,7 +40,6 @@ pub trait BaseFunctionsModule:
     + farm_boosted_yields::FarmBoostedYieldsModule
     + week_timekeeping::WeekTimekeepingModule
     + weekly_rewards_splitting::WeeklyRewardsSplittingModule
-    + weekly_rewards_splitting::ongoing_operation::OngoingOperationModule
     + weekly_rewards_splitting::events::WeeklyRewardsSplittingEventsModule
     + energy_query::EnergyQueryModule
     + utils::UtilsModule
