b6bdac9b553dbb754ac6c46b8e8ee31cfc721fb2
dorin.iancu <dorin.iancu@elrond.com>
2022-10-20 09:30:24 +0300

    rename

---
+++ dex/governance/src/weight.rs
@@ -0,0 +1,59 @@
+elrond_wasm::imports!();
+
+use crate::config;
+
+mod price_provider_proxy {
+    elrond_wasm::imports!();
+
+    #[elrond_wasm::proxy]
+    pub trait PriceProvider {
+        #[endpoint(updateAndGetTokensForGivenPositionWithSafePrice)]
+        fn update_and_get_tokens_for_given_position_with_safe_price(
+            &self,
+            liquidity: BigUint,
+        ) -> MultiValue2<EsdtTokenPayment<Self::Api>, EsdtTokenPayment<Self::Api>>;
+    }
+}
+
+#[elrond_wasm::module]
+pub trait Lib: config::Config {
+    fn get_vote_weight(&self, payment: &EsdtTokenPayment<Self::Api>) -> BigUint {
+        let mex_token_id = self.mex_token_id().get();
+
+        if payment.token_identifier == self.mex_token_id().get() {
+            return payment.amount.clone();
+        }
+
+        if let Some(provider) = self.price_providers().get(&payment.token_identifier) {
+            let call_result: MultiValue2<EsdtTokenPayment<Self::Api>, EsdtTokenPayment<Self::Api>> =
+                self.price_provider_proxy(provider)
+                    .update_and_get_tokens_for_given_position_with_safe_price(
+                        payment.amount.clone(),
+                    )
+                    .execute_on_dest_context();
+            let (token1, token2) = call_result.into_tuple();
+
+            if token1.token_identifier == mex_token_id {
+                return token1.amount;
+            }
+
+            if token2.token_identifier == mex_token_id {
+                return token2.amount;
+            }
+        }
+
+        BigUint::zero()
+    }
+
+    fn send_back(&self, payment: EsdtTokenPayment<Self::Api>) {
+        self.send().direct_esdt(
+            &self.blockchain().get_caller(),
+            &payment.token_identifier,
+            payment.token_nonce,
+            &payment.amount,
+        );
+    }
+
+    #[proxy]
+    fn price_provider_proxy(&self, to: ManagedAddress) -> price_provider_proxy::Proxy<Self::Api>;
+}
