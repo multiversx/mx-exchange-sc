287b0ceedad9704fc651660350a98d8d0241ca40
dorin.iancu <dorin.iancu@elrond.com>
2022-10-21 12:02:39 +0300

    refactor

--- energy-integration/common-modules/weekly-rewards-splitting/src/global_info.rs
+++ energy-integration/common-modules/weekly-rewards-splitting/src/global_info.rs
@@ -16,66 +16,106 @@ pub trait WeeklyRewardsGlobalInfo:
         prev_user_energy: &Energy<Self::Api>,
         current_user_energy: &Energy<Self::Api>,
     ) {
+        self.perform_weekly_update(current_week);
+
+        let new_total_locked_tokens = self
+            .update_and_get_total_tokens_amounts_after_user_energy_update(
+                current_week,
+                prev_user_energy,
+                current_user_energy,
+            );
+
+        let new_total_energy = self.update_and_get_total_energy_amounts_after_user_energy_update(
+            current_week,
+            user_last_active_week,
+            prev_user_energy,
+            current_user_energy,
+        );
+
+        self.emit_update_global_amounts_event(
+            current_week,
+            &new_total_locked_tokens,
+            &new_total_energy,
+        );
+    }
+
+    fn perform_weekly_update(&self, current_week: Week) {
         let last_global_update_mapper = self.last_global_update_week();
         let last_global_update_week = last_global_update_mapper.get();
-        if last_global_update_week != current_week {
-            if last_global_update_week > 0 {
-                let total_energy_prev_week =
-                    self.total_energy_for_week(last_global_update_week).get();
-                let total_tokens_prev_week = self
-                    .total_locked_tokens_for_week(last_global_update_week)
-                    .get();
-
-                let week_diff = current_week - last_global_update_week;
-                let energy_deplete = &total_tokens_prev_week * EPOCHS_IN_WEEK * week_diff as u64;
-                let energy_for_current_week = if total_energy_prev_week >= energy_deplete {
-                    total_energy_prev_week - energy_deplete
-                } else {
-                    BigUint::zero()
-                };
-
-                self.total_energy_for_week(current_week)
-                    .set(&energy_for_current_week);
-                self.total_locked_tokens_for_week(current_week)
-                    .set(&total_tokens_prev_week);
-            }
-
-            last_global_update_mapper.set(current_week);
+        if last_global_update_week == current_week {
+            return;
         }
 
+        last_global_update_mapper.set(current_week);
+
+        if last_global_update_week == 0 {
+            return;
+        }
+
+        let total_energy_prev_week = self.total_energy_for_week(last_global_update_week).get();
+        let total_tokens_prev_week = self
+            .total_locked_tokens_for_week(last_global_update_week)
+            .get();
+
+        let week_diff = current_week - last_global_update_week;
+        let energy_deplete = &total_tokens_prev_week * EPOCHS_IN_WEEK * week_diff as u64;
+        let energy_for_current_week = if total_energy_prev_week >= energy_deplete {
+            total_energy_prev_week - energy_deplete
+        } else {
+            BigUint::zero()
+        };
+
+        self.total_energy_for_week(current_week)
+            .set(&energy_for_current_week);
+        self.total_locked_tokens_for_week(current_week)
+            .set(&total_tokens_prev_week);
+    }
+
+    fn update_and_get_total_tokens_amounts_after_user_energy_update(
+        &self,
+        current_week: Week,
+        prev_user_energy: &Energy<Self::Api>,
+        current_user_energy: &Energy<Self::Api>,
+    ) -> BigUint {
         let total_removed_from_buckets = self.shift_buckets_and_get_removed_token_amount();
         let bucket_pair =
             self.reallocate_bucket_after_energy_update(prev_user_energy, current_user_energy);
 
-        let new_total_locked_tokens =
-            self.total_locked_tokens_for_week(current_week)
-                .update(|total_locked| {
-                    *total_locked -= total_removed_from_buckets;
+        self.total_locked_tokens_for_week(current_week)
+            .update(|total_locked| {
+                *total_locked -= total_removed_from_buckets;
 
-                    let had_prev_energy = bucket_pair.opt_prev_bucket.is_some();
-                    let has_current_energy = bucket_pair.opt_current_bucket.is_some();
-                    if had_prev_energy && has_current_energy {
-                        // usual case of non-zero for both prev and current energy
-                        *total_locked -= prev_user_energy.get_total_locked_tokens();
-                        *total_locked += current_user_energy.get_total_locked_tokens();
-                    } else if had_prev_energy && !has_current_energy {
-                        // only decrease if previous energy > 0,
-                        // otherwise, these tokens were already removed by global shifting
-                        // current not added, as it's 0
-                        *total_locked -= prev_user_energy.get_total_locked_tokens();
-                    } else if !had_prev_energy && has_current_energy {
-                        // if user had 0 energy, but now has non-zero,
-                        // then we have to only add the new tokens, as the old were already deducted
-                        // during the global shifting
-                        *total_locked += current_user_energy.get_total_locked_tokens();
-                    }
-                    // for the case when user had and has no energy, we do nothing
+                let had_prev_energy = bucket_pair.opt_prev_bucket.is_some();
+                let has_current_energy = bucket_pair.opt_current_bucket.is_some();
+                if had_prev_energy && has_current_energy {
+                    // usual case of non-zero for both prev and current energy
+                    *total_locked -= prev_user_energy.get_total_locked_tokens();
+                    *total_locked += current_user_energy.get_total_locked_tokens();
+                } else if had_prev_energy && !has_current_energy {
+                    // only decrease if previous energy > 0,
+                    // otherwise, these tokens were already removed by global shifting
+                    // current not added, as it's 0
+                    *total_locked -= prev_user_energy.get_total_locked_tokens();
+                } else if !had_prev_energy && has_current_energy {
+                    // if user had 0 energy, but now has non-zero,
+                    // then we have to only add the new tokens, as the old were already deducted
+                    // during the global shifting
+                    *total_locked += current_user_energy.get_total_locked_tokens();
+                }
+                // for the case when user had and has no energy, we do nothing
 
-                    (*total_locked).clone()
-                });
+                (*total_locked).clone()
+            })
+    }
 
-        let new_total_energy = self
-            .total_energy_for_week(current_week)
+    fn update_and_get_total_energy_amounts_after_user_energy_update(
+        &self,
+        current_week: Week,
+        user_last_active_week: Week,
+        prev_user_energy: &Energy<Self::Api>,
+        current_user_energy: &Energy<Self::Api>,
+    ) -> BigUint {
+        self.total_energy_for_week(current_week)
             .update(|total_energy| {
                 // revert the 7 * tokens removed in global decrease step
                 if user_last_active_week != current_week {
@@ -86,13 +126,7 @@ pub trait WeeklyRewardsGlobalInfo:
                 *total_energy += current_user_energy.get_energy_amount();
 
                 (*total_energy).clone()
-            });
-
-        self.emit_update_global_amounts_event(
-            current_week,
-            &new_total_locked_tokens,
-            &new_total_energy,
-        )
+            })
     }
 
     #[view(getLastGlobalUpdateWeek)]
