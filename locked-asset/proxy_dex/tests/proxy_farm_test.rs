91882bf698a078453d1a3ad2fd5243f6a1b4ca10
Claudiu Lataretu <claudiu.lataretu@gmail.com>
2022-11-15 17:56:51 +0200

    add unit test to check user energy on claim rewards
    Signed-off-by: Claudiu Lataretu <claudiu.lataretu@gmail.com>

--- locked-asset/proxy_dex/tests/proxy_farm_test.rs
+++ locked-asset/proxy_dex/tests/proxy_farm_test.rs
@@ -18,6 +18,7 @@ fn farm_proxy_actions_test() {
         proxy_dex::contract_obj,
         pair::contract_obj,
         farm::contract_obj,
+        farm_with_locked_rewards::contract_obj,
         energy_factory::contract_obj,
     );
     let first_user = setup.first_user.clone();
@@ -195,5 +196,107 @@ fn farm_proxy_actions_test() {
                 amount: managed_biguint!(USER_BALANCE),
             },
         }),
+    );
+}
+
+#[test]
+fn farm_proxy_claim_energy_test() {
+    let mut setup = ProxySetup::new(
+        proxy_dex::contract_obj,
+        pair::contract_obj,
+        farm::contract_obj,
+        farm_with_locked_rewards::contract_obj,
+        energy_factory::contract_obj,
+    );
+    let first_user = setup.first_user.clone();
+    let farm_locked_addr = setup.farm_locked_wrapper.address_ref().clone();
+
+    //////////////////////////////////////////// ENTER FARM /////////////////////////////////////
+
+    setup
+        .b_mock
+        .execute_esdt_transfer(
+            &first_user,
+            &setup.proxy_wrapper,
+            LOCKED_TOKEN_ID,
+            1,
+            &rust_biguint!(USER_BALANCE),
+            |sc| {
+                sc.enter_farm_proxy_endpoint(managed_address!(&farm_locked_addr));
+            },
+        )
+        .assert_ok();
+
+    // check user balance
+    setup.b_mock.check_nft_balance::<Empty>(
+        &first_user,
+        LOCKED_TOKEN_ID,
+        1,
+        &rust_biguint!(0),
+        None,
+    );
+    setup.b_mock.check_nft_balance(
+        &first_user,
+        WRAPPED_FARM_TOKEN_ID,
+        1,
+        &rust_biguint!(USER_BALANCE),
+        Some(&WrappedFarmTokenAttributes::<DebugApi> {
+            proxy_farming_token: EsdtTokenPayment {
+                token_identifier: managed_token_id!(LOCKED_TOKEN_ID),
+                token_nonce: 1,
+                amount: managed_biguint!(USER_BALANCE),
+            },
+            farm_token: EsdtTokenPayment {
+                token_identifier: managed_token_id!(FARM_LOCKED_TOKEN_ID),
+                token_nonce: 1,
+                amount: managed_biguint!(USER_BALANCE),
+            },
+        }),
+    );
+
+    // check proxy balance
+    setup
+        .b_mock
+        .check_nft_balance::<FarmTokenAttributes<DebugApi>>(
+            setup.proxy_wrapper.address_ref(),
+            FARM_LOCKED_TOKEN_ID,
+            1,
+            &rust_biguint!(USER_BALANCE),
+            None,
+        );
+
+    // check farm balance
+    setup.b_mock.check_esdt_balance(
+        setup.farm_locked_wrapper.address_ref(),
+        MEX_TOKEN_ID,
+        &rust_biguint!(USER_BALANCE),
+    );
+
+    setup.b_mock.set_block_nonce(100);
+
+    //////////////////////////////////////////// CLAIM REWARDS /////////////////////////////////////
+
+    // claim rewards with half position
+    setup
+        .b_mock
+        .execute_esdt_transfer(
+            &first_user,
+            &setup.proxy_wrapper,
+            WRAPPED_FARM_TOKEN_ID,
+            1,
+            &rust_biguint!(USER_BALANCE),
+            |sc| {
+                sc.claim_rewards_proxy(managed_address!(&farm_locked_addr));
+            },
+        )
+        .assert_ok();
+
+    // check user balance
+    setup.b_mock.check_nft_balance::<Empty>(
+        &first_user,
+        LOCKED_TOKEN_ID,
+        1,
+        &(rust_biguint!(PER_BLOCK_REWARD_AMOUNT) * 100u32),
+        None,
     );
 }
