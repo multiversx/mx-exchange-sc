d5e0ed6edd8cfd0b42f228e77d710c2d1db756cb
dorin.iancu <dorin.iancu@elrond.com>
2022-11-17 11:17:17 +0200

    cancel unbond test

--- locked-asset/token-unstake/tests/token_unstake_test.rs
+++ locked-asset/token-unstake/tests/token_unstake_test.rs
@@ -13,6 +13,16 @@ use token_unstake::{
 };
 use token_unstake_setup::*;
 
+pub struct ResultWrapper<EnergyFactoryBuilder, UnstakeScBuilder>
+where
+    EnergyFactoryBuilder: 'static + Copy + Fn() -> energy_factory::ContractObj<DebugApi>,
+    UnstakeScBuilder: 'static + Copy + Fn() -> token_unstake::ContractObj<DebugApi>,
+{
+    pub setup: TokenUnstakeSetup<EnergyFactoryBuilder, UnstakeScBuilder>,
+    pub balance_after_second_reduce: num_bigint::BigUint,
+    pub final_penalty_amount: num_bigint::BigUint,
+}
+
 #[test]
 fn init_token_unstake_test() {
     let _ = TokenUnstakeSetup::new(energy_factory::contract_obj, token_unstake::contract_obj);
@@ -20,8 +30,100 @@ fn init_token_unstake_test() {
 
 #[test]
 fn unstake_sc_fees_merging_and_unbond_test() {
-    let mut setup =
-        TokenUnstakeSetup::new(energy_factory::contract_obj, token_unstake::contract_obj);
+    let result = unbond_test_common(energy_factory::contract_obj, token_unstake::contract_obj);
+    let (mut setup, balance_after_second_reduce, final_penalty_amount) = (
+        result.setup,
+        result.balance_after_second_reduce,
+        result.final_penalty_amount,
+    );
+    let first_user = setup.first_user.clone();
+    let half_balance = USER_BALANCE / 2;
+
+    // user try unbond early
+    setup
+        .unbond(&first_user)
+        .assert_user_error("Nothing to unbond");
+
+    // unbond epochs pass
+    setup.b_mock.set_block_epoch(10 + UNBOND_EPOCHS);
+
+    // unbond ok
+    setup.unbond(&first_user).assert_ok();
+
+    let final_user_balance = &balance_after_second_reduce - &final_penalty_amount;
+    let user_balance_after_unbond = rust_biguint!(half_balance) + final_user_balance;
+    setup
+        .b_mock
+        .check_esdt_balance(&first_user, BASE_ASSET_TOKEN_ID, &user_balance_after_unbond);
+
+    // check fees added correctly
+    // nonce is 3, since we already had a token with this unlock epoch
+    setup.b_mock.check_nft_balance(
+        &setup.fees_collector_mock,
+        LOCKED_TOKEN_ID,
+        3,
+        &(final_penalty_amount / 2u64),
+        Some(&LockedTokenAttributes::<DebugApi> {
+            original_token_id: managed_token_id_wrapped!(BASE_ASSET_TOKEN_ID),
+            original_token_nonce: 0,
+            unlock_epoch: LOCK_OPTIONS[0],
+        }),
+    );
+
+    let user_energy = setup.get_user_energy(&first_user);
+    assert_eq!(user_energy, rust_biguint!(0));
+}
+
+#[test]
+fn cancel_unbond_test() {
+    let result = unbond_test_common(energy_factory::contract_obj, token_unstake::contract_obj);
+    let (mut setup, balance_after_second_reduce, _final_penalty_amount) = (
+        result.setup,
+        result.balance_after_second_reduce,
+        result.final_penalty_amount,
+    );
+    let first_user = setup.first_user.clone();
+
+    setup.cancel_unbond(&first_user).assert_ok();
+
+    // check user entries after unbond
+    setup
+        .b_mock
+        .execute_query(&setup.unstake_sc_wrapper, |sc| {
+            assert!(sc
+                .unlocked_tokens_for_user(&managed_address!(&first_user))
+                .is_empty());
+        })
+        .assert_ok();
+
+    // check user balance - they get the locked token back
+    setup.b_mock.check_nft_balance(
+        &first_user,
+        LOCKED_TOKEN_ID,
+        3,
+        &balance_after_second_reduce,
+        Some(&LockedTokenAttributes::<DebugApi> {
+            original_token_id: managed_token_id_wrapped!(BASE_ASSET_TOKEN_ID),
+            original_token_nonce: 0,
+            unlock_epoch: LOCK_OPTIONS[0],
+        }),
+    );
+
+    // check energy was added back - current epoch is 10
+    let user_energy = setup.get_user_energy(&first_user);
+    let expected_energy = rust_biguint!(LOCK_OPTIONS[0] - 10) * balance_after_second_reduce;
+    assert_eq!(user_energy, expected_energy);
+}
+
+fn unbond_test_common<EnergyFactoryBuilder, UnstakeScBuilder>(
+    energy_factory_builder: EnergyFactoryBuilder,
+    unstake_sc_builder: UnstakeScBuilder,
+) -> ResultWrapper<EnergyFactoryBuilder, UnstakeScBuilder>
+where
+    EnergyFactoryBuilder: 'static + Copy + Fn() -> energy_factory::ContractObj<DebugApi>,
+    UnstakeScBuilder: 'static + Copy + Fn() -> token_unstake::ContractObj<DebugApi>,
+{
+    let mut setup = TokenUnstakeSetup::new(energy_factory_builder, unstake_sc_builder);
     let first_user = setup.first_user.clone();
     let half_balance = USER_BALANCE / 2;
 
@@ -165,7 +267,7 @@ fn unstake_sc_fees_merging_and_unbond_test() {
                 locked_tokens: EsdtTokenPayment::new(
                     managed_token_id!(LOCKED_TOKEN_ID),
                     3,
-                    to_managed_biguint(balance_after_second_reduce),
+                    to_managed_biguint(balance_after_second_reduce.clone()),
                 ),
                 unlocked_tokens: EsdtTokenPayment::new(
                     managed_token_id!(BASE_ASSET_TOKEN_ID),
@@ -193,33 +295,9 @@ fn unstake_sc_fees_merging_and_unbond_test() {
         }),
     );
 
-    // user try unbond early
-    setup
-        .unbond(&first_user)
-        .assert_user_error("Nothing to unbond");
-
-    // unbond epochs pass
-    setup.b_mock.set_block_epoch(10 + UNBOND_EPOCHS);
-
-    // unbond ok
-    setup.unbond(&first_user).assert_ok();
-
-    let user_balance_after_unbond = rust_biguint!(half_balance) + final_user_balance;
-    setup
-        .b_mock
-        .check_esdt_balance(&first_user, BASE_ASSET_TOKEN_ID, &user_balance_after_unbond);
-
-    // check fees added correctly
-    // nonce is 3, since we already had a token with this unlock epoch
-    setup.b_mock.check_nft_balance(
-        &setup.fees_collector_mock,
-        LOCKED_TOKEN_ID,
-        3,
-        &(final_penalty_amount / 2u64),
-        Some(&LockedTokenAttributes::<DebugApi> {
-            original_token_id: managed_token_id_wrapped!(BASE_ASSET_TOKEN_ID),
-            original_token_nonce: 0,
-            unlock_epoch: LOCK_OPTIONS[0],
-        }),
-    );
+    ResultWrapper {
+        setup,
+        balance_after_second_reduce,
+        final_penalty_amount,
+    }
 }
