94f70a7f1936c3874ca7235db1903185fe51e8f3
dorin.iancu <dorin.iancu@elrond.com>
2022-11-17 15:05:56 +0200

    remove unused endpoint

--- energy-integration/fees-collector/src/lib.rs
+++ energy-integration/fees-collector/src/lib.rs
@@ -4,12 +4,7 @@ elrond_wasm::imports!();
 
 use common_types::{PaymentsVec, Week};
 use core::marker::PhantomData;
-use energy_query::Energy;
 use weekly_rewards_splitting::base_impl::WeeklyRewardsSplittingTraitsModule;
-
-use elrond_wasm_modules::ongoing_operation::{
-    CONTINUE_OP, DEFAULT_MIN_GAS_TO_SAVE_PROGRESS, STOP_OP,
-};
 
 pub mod config;
 pub mod events;
@@ -28,7 +23,6 @@ pub trait FeesCollector:
     + energy_query::EnergyQueryModule
     + week_timekeeping::WeekTimekeepingModule
     + elrond_wasm_modules::pause::PauseModule
-    + elrond_wasm_modules::ongoing_operation::OngoingOperationModule
 {
     #[init]
     fn init(&self) {
@@ -39,6 +33,7 @@ pub trait FeesCollector:
     #[endpoint(claimRewards)]
     fn claim_rewards(&self) -> PaymentsVec<Self::Api> {
         require!(self.not_paused(), "Cannot claim while paused");
+
         let caller = self.blockchain().get_caller();
         let wrapper = FeesCollectorWrapper::new();
         let rewards = self.claim_multi(&wrapper, &caller);
@@ -47,61 +42,6 @@ pub trait FeesCollector:
         }
 
         rewards
-    }
-
-    /// Accepts pairs of (user address, energy amount, total locked tokens).
-    /// Sets the given amounts for the user's positions,
-    /// and recomputes the global amounts.
-    ///
-    /// Returns whether the operation was fully completed.
-    /// If not, it also returns the last processed index.
-    #[only_owner]
-    #[endpoint(recomputeEnergy)]
-    fn recompute_energy(
-        &self,
-        arg_pairs: MultiValueEncoded<MultiValue3<ManagedAddress, BigUint, BigUint>>,
-    ) -> MultiValue2<OperationCompletionStatus, OptionalValue<usize>> {
-        require!(self.is_paused(), "May only recompute while paused");
-
-        let current_week = self.get_current_week();
-        let current_epoch = self.blockchain().get_block_epoch();
-
-        let mut iter = arg_pairs.into_iter().enumerate();
-        let mut last_processed_index = 0;
-
-        let run_result = self.run_while_it_has_gas(DEFAULT_MIN_GAS_TO_SAVE_PROGRESS, || match iter
-            .next()
-        {
-            Some((index, multi_value)) => {
-                let (user, energy, total_locked) = multi_value.into_tuple();
-                let energy_entry = Energy::new(BigInt::from(energy), current_epoch, total_locked);
-
-                self.current_claim_progress(&user).update(|claim_progress| {
-                    self.update_user_energy_for_current_week(
-                        &user,
-                        current_week,
-                        &energy_entry,
-                        Some(claim_progress.clone()),
-                    );
-
-                    if claim_progress.week == current_week {
-                        claim_progress.energy = energy_entry;
-                    }
-                });
-
-                last_processed_index = index;
-
-                CONTINUE_OP
-            }
-            None => STOP_OP,
-        });
-
-        match run_result {
-            OperationCompletionStatus::Completed => (run_result, OptionalValue::None).into(),
-            OperationCompletionStatus::InterruptedBeforeOutOfGas => {
-                (run_result, OptionalValue::Some(last_processed_index)).into()
-            }
-        }
     }
 }
 
