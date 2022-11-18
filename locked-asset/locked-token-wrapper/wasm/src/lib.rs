5eeb91830aa511da7ac813806ffa9159da0a71a9
dorin.iancu <dorin.iancu@elrond.com>
2022-11-18 12:51:50 +0200

    implementation

--- locked-asset/locked-token-wrapper/wasm/src/lib.rs
+++ locked-asset/locked-token-wrapper/wasm/src/lib.rs
@@ -7,7 +7,14 @@
 elrond_wasm_node::wasm_endpoints! {
     locked_token_wrapper
     (
+        callBack
+        getEnergyFactoryAddress
+        getLockedTokenId
+        getWrappedTokenId
+        issueWrappedToken
+        setEnergyFactoryAddress
+        setTransferRoleWrappedToken
+        unwrapLockedToken
+        wrapLockedToken
     )
 }
-
-elrond_wasm_node::wasm_empty_callback! {}
