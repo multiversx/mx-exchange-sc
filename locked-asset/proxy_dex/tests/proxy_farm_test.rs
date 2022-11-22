ee83f328d71dcd1d1144b6757221bd74ad89dfe4
Sorin Petreasca <petreasca.sorin@gmail.com>
2022-11-22 12:00:01 +0200

    proxy dex partial exit test

--- locked-asset/proxy_dex/tests/proxy_farm_test.rs
+++ locked-asset/proxy_dex/tests/proxy_farm_test.rs
@@ -345,3 +345,179 @@ fn farm_proxy_claim_energy_test() {
         })
         .assert_ok();
 }
+
+#[test]
+fn farm_proxy_partial_exit_test() {
+    let mut setup = ProxySetup::new(
+        proxy_dex::contract_obj,
+        pair::contract_obj,
+        farm_with_locked_rewards::contract_obj,
+        energy_factory::contract_obj,
+    );
+    let first_user = setup.first_user.clone();
+    let farm_locked_addr = setup.farm_locked_wrapper.address_ref().clone();
+
+    //////////////////////////////////////////// ENTER FARM /////////////////////////////////////
+
+    let current_epoch = 5;
+    setup.b_mock.set_block_epoch(current_epoch);
+
+    let expected_energy = rust_biguint!(LOCK_OPTIONS[0] - current_epoch) * USER_BALANCE;
+    setup
+        .b_mock
+        .execute_query(&setup.simple_lock_wrapper, |sc| {
+            let managed_result = sc.get_energy_amount_for_user(managed_address!(&first_user));
+            let result = to_rust_biguint(managed_result);
+            assert_eq!(result, expected_energy);
+        })
+        .assert_ok();
+
+    setup
+        .b_mock
+        .execute_esdt_transfer(
+            &first_user,
+            &setup.proxy_wrapper,
+            LOCKED_TOKEN_ID,
+            1,
+            &rust_biguint!(USER_BALANCE),
+            |sc| {
+                sc.enter_farm_proxy_endpoint(managed_address!(&farm_locked_addr));
+            },
+        )
+        .assert_ok();
+
+    let expected_energy = rust_biguint!(LOCK_OPTIONS[0] - current_epoch) * USER_BALANCE;
+    setup
+        .b_mock
+        .execute_query(&setup.simple_lock_wrapper, |sc| {
+            let managed_result = sc.get_energy_amount_for_user(managed_address!(&first_user));
+            let result = to_rust_biguint(managed_result);
+            assert_eq!(result, expected_energy);
+        })
+        .assert_ok();
+
+    // check user balance
+    setup.b_mock.check_nft_balance::<Empty>(
+        &first_user,
+        LOCKED_TOKEN_ID,
+        1,
+        &rust_biguint!(0),
+        None,
+    );
+    setup.b_mock.check_nft_balance(
+        &first_user,
+        WRAPPED_FARM_TOKEN_ID,
+        1,
+        &rust_biguint!(USER_BALANCE),
+        Some(&WrappedFarmTokenAttributes::<DebugApi> {
+            proxy_farming_token: EsdtTokenPayment {
+                token_identifier: managed_token_id!(LOCKED_TOKEN_ID),
+                token_nonce: 1,
+                amount: managed_biguint!(USER_BALANCE),
+            },
+            farm_token: EsdtTokenPayment {
+                token_identifier: managed_token_id!(FARM_LOCKED_TOKEN_ID),
+                token_nonce: 1,
+                amount: managed_biguint!(USER_BALANCE),
+            },
+        }),
+    );
+
+    // check proxy balance
+    setup
+        .b_mock
+        .check_nft_balance::<FarmTokenAttributes<DebugApi>>(
+            setup.proxy_wrapper.address_ref(),
+            FARM_LOCKED_TOKEN_ID,
+            1,
+            &rust_biguint!(USER_BALANCE),
+            None,
+        );
+
+    // check farm balance
+    setup.b_mock.check_esdt_balance(
+        setup.farm_locked_wrapper.address_ref(),
+        MEX_TOKEN_ID,
+        &rust_biguint!(USER_BALANCE),
+    );
+
+    setup.b_mock.set_block_nonce(100);
+
+    //////////////////////////////////////////// PARTIAL EXIT /////////////////////////////////////
+
+    // partial exit farm with 50%
+    setup
+        .b_mock
+        .execute_esdt_transfer(
+            &first_user,
+            &setup.proxy_wrapper,
+            WRAPPED_FARM_TOKEN_ID,
+            1,
+            &rust_biguint!(USER_BALANCE),
+            |sc| {
+                sc.exit_farm_proxy(
+                    managed_address!(&farm_locked_addr),
+                    managed_biguint!(USER_BALANCE / 2),
+                );
+            },
+        )
+        .assert_ok();
+
+    // check user balance - base rewards for partial position (50%) + remaining balance (50%)
+    // rewards for the full position only applies for the boosted rewards
+    setup.b_mock.check_nft_balance::<Empty>(
+        &first_user,
+        LOCKED_TOKEN_ID,
+        1,
+        &(rust_biguint!(PER_BLOCK_REWARD_AMOUNT * 100 / 2 + USER_BALANCE / 2)),
+        None,
+    );
+
+    let new_user_balance = USER_BALANCE + rust_biguint!(PER_BLOCK_REWARD_AMOUNT) * 100u32 / 2u32;
+    let expected_energy = rust_biguint!(LOCK_OPTIONS[0] - current_epoch) * new_user_balance;
+    setup
+        .b_mock
+        .execute_query(&setup.simple_lock_wrapper, |sc| {
+            let managed_result = sc.get_energy_amount_for_user(managed_address!(&first_user));
+            let result = to_rust_biguint(managed_result);
+            assert_eq!(result, expected_energy);
+        })
+        .assert_ok();
+
+    setup.b_mock.check_nft_balance(
+        &first_user,
+        WRAPPED_FARM_TOKEN_ID,
+        1,
+        &rust_biguint!(USER_BALANCE / 2),
+        Some(&WrappedFarmTokenAttributes::<DebugApi> {
+            proxy_farming_token: EsdtTokenPayment {
+                token_identifier: managed_token_id!(LOCKED_TOKEN_ID),
+                token_nonce: 1,
+                amount: managed_biguint!(USER_BALANCE),
+            },
+            farm_token: EsdtTokenPayment {
+                token_identifier: managed_token_id!(FARM_LOCKED_TOKEN_ID),
+                token_nonce: 1,
+                amount: managed_biguint!(USER_BALANCE),
+            },
+        }),
+    );
+
+    // check proxy balance
+    setup
+        .b_mock
+        .check_nft_balance::<FarmTokenAttributes<DebugApi>>(
+            setup.proxy_wrapper.address_ref(),
+            FARM_LOCKED_TOKEN_ID,
+            1,
+            &rust_biguint!(USER_BALANCE / 2),
+            None,
+        );
+
+    // check farm balance
+    setup.b_mock.check_esdt_balance(
+        setup.farm_locked_wrapper.address_ref(),
+        MEX_TOKEN_ID,
+        &rust_biguint!(USER_BALANCE / 2),
+    );
+}
