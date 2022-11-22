00c4e0ce25287148d9b81b98f790beb1f598fe16
dorin.iancu <dorin.iancu@elrond.com>
2022-11-22 16:53:48 +0200

    remove unused import

--- locked-asset/token-unstake/tests/token_unstake_setup/mod.rs
+++ locked-asset/token-unstake/tests/token_unstake_setup/mod.rs
@@ -18,7 +18,6 @@ use energy_factory::{
     energy::EnergyModule, unlock_with_penalty::UnlockWithPenaltyModule, unstake::UnstakeModule,
     SimpleLockEnergy,
 };
-use energy_query::Energy;
 use simple_lock::locked_token::LockedTokenModule;
 
 use fees_collector_mock::*;
