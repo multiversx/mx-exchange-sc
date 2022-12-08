ac1f591386affaff3972ff68023c21d2be08cc57
Sorin Petreasca <petreasca.sorin@gmail.com>
2022-12-08 16:04:10 +0200

    unwrap functionality for legacy locked token

--- locked-asset/locked-token-wrapper/src/lib.rs
+++ locked-asset/locked-token-wrapper/src/lib.rs
@@ -14,12 +14,21 @@ pub trait LockedTokenWrapper:
     + utils::UtilsModule
 {
     #[init]
-    fn init(&self, locked_token_id: TokenIdentifier, energy_factory_address: ManagedAddress) {
-        self.require_valid_token_id(&locked_token_id);
+    fn init(
+        &self,
+        old_locked_token_id: TokenIdentifier,
+        new_locked_token_id: TokenIdentifier,
+        energy_factory_address: ManagedAddress,
+    ) {
+        self.require_valid_token_id(&old_locked_token_id);
+        self.require_valid_token_id(&new_locked_token_id);
         self.require_sc_address(&energy_factory_address);
 
+        if self.old_locked_token().is_empty() {
+            self.old_locked_token().set_token_id(old_locked_token_id);
+        }
         if self.locked_token().is_empty() {
-            self.locked_token().set_token_id(locked_token_id);
+            self.locked_token().set_token_id(new_locked_token_id);
         }
         self.energy_factory_address().set(&energy_factory_address);
     }
@@ -29,12 +38,23 @@ pub trait LockedTokenWrapper:
     fn wrap_locked_token_endpoint(&self) -> EsdtTokenPayment {
         let payment = self.call_value().single_esdt();
         let caller = self.blockchain().get_caller();
-        self.deduct_energy_from_sender(
-            caller.clone(),
-            &ManagedVec::from_single_item(payment.clone()),
-        );
 
-        self.wrap_locked_token_and_send(&caller, payment)
+        if payment.token_identifier == self.locked_token().get_token_id() {
+            self.deduct_energy_from_sender(
+                caller.clone(),
+                &ManagedVec::from_single_item(payment.clone()),
+            );
+            self.wrap_locked_token_and_send(&caller, payment)
+        } else if payment.token_identifier == self.old_locked_token().get_token_id() {
+            self.deduct_old_token_energy_from_sender(
+                caller.clone(),
+                &ManagedVec::from_single_item(payment.clone()),
+            );
+
+            self.wrap_locked_token_and_send(&caller, payment)
+        } else {
+            sc_panic!("Bad payment token");
+        }
     }
 
     #[payable("*")]
@@ -44,10 +64,18 @@ pub trait LockedTokenWrapper:
         let caller = self.blockchain().get_caller();
         let original_locked_tokens = self.unwrap_locked_token(payment);
 
-        self.add_energy_to_destination(
-            caller.clone(),
-            &ManagedVec::from_single_item(original_locked_tokens.clone()),
-        );
+        if original_locked_tokens.token_identifier == self.locked_token().get_token_id() {
+            self.add_energy_to_destination(
+                caller.clone(),
+                &ManagedVec::from_single_item(original_locked_tokens.clone()),
+            );
+        } else if original_locked_tokens.token_identifier == self.old_locked_token().get_token_id()
+        {
+            self.add_old_token_energy_to_destination(
+                caller.clone(),
+                &ManagedVec::from_single_item(original_locked_tokens.clone()),
+            );
+        }
 
         self.send().direct_esdt(
             &caller,
