3203ce1d8db5422dafe002875679cb1e2f820265
Sorin Petreasca <90727972+psorinionut@users.noreply.github.com>
2022-12-16 16:05:27 +0200

    Update common/modules/legacy_token_decode_module/src/lib.rs
    Co-authored-by: dorin-iancu <72252689+dorin-iancu@users.noreply.github.com>

--- common/modules/legacy_token_decode_module/src/lib.rs
+++ common/modules/legacy_token_decode_module/src/lib.rs
@@ -13,17 +13,12 @@ pub trait LegacyTokenDecodeModule: utils::UtilsModule {
         token_id: &TokenIdentifier,
         token_nonce: Nonce,
     ) -> OldLockedTokenAttributes<Self::Api> {
-        let attributes: OldLockedTokenAttributes<Self::Api> =
             if token_nonce < LOCKED_TOKEN_ACTIVATION_NONCE {
                 let initial_attributes: InitialOldLockedTokenAttributes<Self::Api> =
                     self.get_token_attributes(token_id, token_nonce);
                 initial_attributes.migrate_to_new_attributes()
             } else {
-                let updated_attributes: OldLockedTokenAttributes<Self::Api> =
-                    self.get_token_attributes(token_id, token_nonce);
-                updated_attributes
-            };
-
-        attributes
+                    self.get_token_attributes(token_id, token_nonce)
+            }
     }
 }
