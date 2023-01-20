1bc1057a4df9e849245575304565e4c49a0b34af
Andrei Marinica <andrei.marinica@elrond.com>
2023-01-20 13:04:03 +0200

    mandos folders renamed to scenarios

--- dex/tests/dex_scenario_go_test.rs
+++ dex/tests/dex_scenario_go_test.rs
@@ -1,201 +1,201 @@
 #[test]
 fn add_liquidity_go() {
-    multiversx_sc_scenario::run_go("mandos/add_liquidity.scen.json");
+    multiversx_sc_scenario::run_go("scenarios/add_liquidity.scen.json");
 }
 
 #[test]
 fn calculate_rewards_for_given_position_go() {
-    multiversx_sc_scenario::run_go("mandos/calculate_rewards_for_given_position.scen.json");
+    multiversx_sc_scenario::run_go("scenarios/calculate_rewards_for_given_position.scen.json");
 }
 
 #[test]
 fn calculate_rewards_for_given_position_after_compound_go() {
     multiversx_sc_scenario::run_go(
-        "mandos/calculate_rewards_for_given_position_after_compound.scen.json",
+        "scenarios/calculate_rewards_for_given_position_after_compound.scen.json",
     );
 }
 
 #[test]
 fn check_fee_disabled_after_swap_go() {
-    multiversx_sc_scenario::run_go("mandos/check_fee_disabled_after_swap.scen.json");
+    multiversx_sc_scenario::run_go("scenarios/check_fee_disabled_after_swap.scen.json");
 }
 
 #[test]
 fn check_fee_enabled_after_swap_go() {
-    multiversx_sc_scenario::run_go("mandos/check_fee_enabled_after_swap.scen.json");
+    multiversx_sc_scenario::run_go("scenarios/check_fee_enabled_after_swap.scen.json");
 }
 
 #[test]
 fn claim_rewards_go() {
-    multiversx_sc_scenario::run_go("mandos/claim_rewards.scen.json");
+    multiversx_sc_scenario::run_go("scenarios/claim_rewards.scen.json");
 }
 
 #[test]
 fn complete_setup_go() {
-    multiversx_sc_scenario::run_go("mandos/complete_setup.scen.json");
+    multiversx_sc_scenario::run_go("scenarios/complete_setup.scen.json");
 }
 
 #[test]
 fn compound_rewards_go() {
-    multiversx_sc_scenario::run_go("mandos/compound_rewards.scen.json");
+    multiversx_sc_scenario::run_go("scenarios/compound_rewards.scen.json");
 }
 
 #[test]
 fn create_pair_twice_go() {
-    multiversx_sc_scenario::run_go("mandos/create_pair_twice.scen.json");
+    multiversx_sc_scenario::run_go("scenarios/create_pair_twice.scen.json");
 }
 
 #[test]
 fn enter_farm_go() {
-    multiversx_sc_scenario::run_go("mandos/enter_farm.scen.json");
+    multiversx_sc_scenario::run_go("scenarios/enter_farm.scen.json");
 }
 
 #[test]
 fn enter_farm_with_merge_tokens_go() {
-    multiversx_sc_scenario::run_go("mandos/enter_farm_with_merge_tokens.scen.json");
+    multiversx_sc_scenario::run_go("scenarios/enter_farm_with_merge_tokens.scen.json");
 }
 
 #[test]
 fn enter_mex_farm_go() {
-    multiversx_sc_scenario::run_go("mandos/enter_mex_farm.scen.json");
+    multiversx_sc_scenario::run_go("scenarios/enter_mex_farm.scen.json");
 }
 
 #[test]
 fn exit_farm_go() {
-    multiversx_sc_scenario::run_go("mandos/exit_farm.scen.json");
+    multiversx_sc_scenario::run_go("scenarios/exit_farm.scen.json");
 }
 
 #[test]
 fn exit_farm_too_soon_go() {
-    multiversx_sc_scenario::run_go("mandos/exit_farm_too_soon.scen.json");
+    multiversx_sc_scenario::run_go("scenarios/exit_farm_too_soon.scen.json");
 }
 
 #[test]
 fn exit_mex_farm_go() {
-    multiversx_sc_scenario::run_go("mandos/exit_mex_farm.scen.json");
+    multiversx_sc_scenario::run_go("scenarios/exit_mex_farm.scen.json");
 }
 
 #[test]
 fn farm_reward_distr_scen_1_go() {
-    multiversx_sc_scenario::run_go("mandos/farm_reward_distr_scen_1.scen.json");
+    multiversx_sc_scenario::run_go("scenarios/farm_reward_distr_scen_1.scen.json");
 }
 
 #[test]
 fn farm_reward_distr_scen_2_go() {
-    multiversx_sc_scenario::run_go("mandos/farm_reward_distr_scen_2.scen.json");
+    multiversx_sc_scenario::run_go("scenarios/farm_reward_distr_scen_2.scen.json");
 }
 
 #[test]
 fn farm_reward_distr_scen_3_go() {
-    multiversx_sc_scenario::run_go("mandos/farm_reward_distr_scen_3.scen.json");
+    multiversx_sc_scenario::run_go("scenarios/farm_reward_distr_scen_3.scen.json");
 }
 
 #[test]
 fn farm_reward_distr_scen_4_go() {
-    multiversx_sc_scenario::run_go("mandos/farm_reward_distr_scen_4.scen.json");
+    multiversx_sc_scenario::run_go("scenarios/farm_reward_distr_scen_4.scen.json");
 }
 
 #[test]
 fn farm_with_egld_token_go() {
-    multiversx_sc_scenario::run_go("mandos/farm_with_egld_token.scen.json");
+    multiversx_sc_scenario::run_go("scenarios/farm_with_egld_token.scen.json");
 }
 
 #[test]
 fn farm_wrong_lp_token_go() {
-    multiversx_sc_scenario::run_go("mandos/farm_wrong_lp_token.scen.json");
+    multiversx_sc_scenario::run_go("scenarios/farm_wrong_lp_token.scen.json");
 }
 
 #[test]
 fn get_amounts_go() {
-    multiversx_sc_scenario::run_go("mandos/get_amounts.scen.json");
+    multiversx_sc_scenario::run_go("scenarios/get_amounts.scen.json");
 }
 
 #[test]
 fn get_amounts_no_liquidity_go() {
-    multiversx_sc_scenario::run_go("mandos/get_amounts_no_liquidity.scen.json");
+    multiversx_sc_scenario::run_go("scenarios/get_amounts_no_liquidity.scen.json");
 }
 
 #[test]
 fn get_pair_non_existent_go() {
-    multiversx_sc_scenario::run_go("mandos/get_pair_non_existent.scen.json");
+    multiversx_sc_scenario::run_go("scenarios/get_pair_non_existent.scen.json");
 }
 
 #[test]
 fn get_pair_views_go() {
-    multiversx_sc_scenario::run_go("mandos/get_pair_views.scen.json");
+    multiversx_sc_scenario::run_go("scenarios/get_pair_views.scen.json");
 }
 
 #[test]
 fn merge_tokens_go() {
-    multiversx_sc_scenario::run_go("mandos/merge_tokens.scen.json");
+    multiversx_sc_scenario::run_go("scenarios/merge_tokens.scen.json");
 }
 
 #[test]
 fn owner_pause_farm_go() {
-    multiversx_sc_scenario::run_go("mandos/owner_pause_farm.scen.json");
+    multiversx_sc_scenario::run_go("scenarios/owner_pause_farm.scen.json");
 }
 
 #[test]
 fn owner_resume_farm_go() {
-    multiversx_sc_scenario::run_go("mandos/owner_resume_farm.scen.json");
+    multiversx_sc_scenario::run_go("scenarios/owner_resume_farm.scen.json");
 }
 
 #[test]
 fn remove_liquidity_go() {
-    multiversx_sc_scenario::run_go("mandos/remove_liquidity.scen.json");
+    multiversx_sc_scenario::run_go("scenarios/remove_liquidity.scen.json");
 }
 
 #[test]
 fn remove_liquidity_and_buyback_and_burn_token_go() {
-    multiversx_sc_scenario::run_go("mandos/remove_liquidity_and_buyback_and_burn_token.scen.json");
+    multiversx_sc_scenario::run_go("scenarios/remove_liquidity_and_buyback_and_burn_token.scen.json");
 }
 
 #[test]
 fn remove_liquidity_twice_go() {
-    multiversx_sc_scenario::run_go("mandos/remove_liquidity_twice.scen.json");
+    multiversx_sc_scenario::run_go("scenarios/remove_liquidity_twice.scen.json");
 }
 
 #[test]
 fn remove_pair_go() {
-    multiversx_sc_scenario::run_go("mandos/remove_pair.scen.json");
+    multiversx_sc_scenario::run_go("scenarios/remove_pair.scen.json");
 }
 
 #[test]
 fn router_pause_self_go() {
-    multiversx_sc_scenario::run_go("mandos/router_pause_self.scen.json");
+    multiversx_sc_scenario::run_go("scenarios/router_pause_self.scen.json");
 }
 
 #[test]
 fn router_resume_self_go() {
-    multiversx_sc_scenario::run_go("mandos/router_resume_self.scen.json");
+    multiversx_sc_scenario::run_go("scenarios/router_resume_self.scen.json");
 }
 
 #[test]
 fn swap_fixed_input_go() {
-    multiversx_sc_scenario::run_go("mandos/swap_fixed_input.scen.json");
+    multiversx_sc_scenario::run_go("scenarios/swap_fixed_input.scen.json");
 }
 
 #[test]
 fn swap_fixed_input_after_removed_liquidity_go() {
-    multiversx_sc_scenario::run_go("mandos/swap_fixed_input_after_removed_liquidity.scen.json");
+    multiversx_sc_scenario::run_go("scenarios/swap_fixed_input_after_removed_liquidity.scen.json");
 }
 
 #[test]
 fn swap_fixed_output_go() {
-    multiversx_sc_scenario::run_go("mandos/swap_fixed_output.scen.json");
+    multiversx_sc_scenario::run_go("scenarios/swap_fixed_output.scen.json");
 }
 
 #[test]
 fn swap_same_token_go() {
-    multiversx_sc_scenario::run_go("mandos/swap_same_token.scen.json");
+    multiversx_sc_scenario::run_go("scenarios/swap_same_token.scen.json");
 }
 
 #[test]
 fn swap_wrong_token_go() {
-    multiversx_sc_scenario::run_go("mandos/swap_wrong_token.scen.json");
+    multiversx_sc_scenario::run_go("scenarios/swap_wrong_token.scen.json");
 }
 
 #[test]
 fn upgrade_contract_go() {
-    multiversx_sc_scenario::run_go("mandos/upgrade_contract.scen.json");
+    multiversx_sc_scenario::run_go("scenarios/upgrade_contract.scen.json");
 }
--- locked-asset/tests/distribution_scenario_go_test.rs
+++ locked-asset/tests/distribution_scenario_go_test.rs
@@ -1,138 +1,138 @@
 // #[test]
 // fn add_liquidity_proxy_go() {
-//     multiversx_sc_scenario::run_go("mandos/add_liquidity_proxy.scen.json");
+//     multiversx_sc_scenario::run_go("scenarios/add_liquidity_proxy.scen.json");
 // }
 
 // #[test]
 // fn claim_locked_assets_basic_go() {
-//     multiversx_sc_scenario::run_go("mandos/claim_locked_assets_basic.scen.json");
+//     multiversx_sc_scenario::run_go("scenarios/claim_locked_assets_basic.scen.json");
 // }
 
 // #[test]
 // fn claim_mex_rewards_proxy_after_mint_rewards_go() {
-//     multiversx_sc_scenario::run_go("mandos/claim_mex_rewards_proxy_after_mint_rewards.scen.json");
+//     multiversx_sc_scenario::run_go("scenarios/claim_mex_rewards_proxy_after_mint_rewards.scen.json");
 // }
 
 // #[test]
 // fn claim_only_last_four_go() {
-//     multiversx_sc_scenario::run_go("mandos/claim_only_last_four.scen.json");
+//     multiversx_sc_scenario::run_go("scenarios/claim_only_last_four.scen.json");
 // }
 
 // #[test]
 // fn claim_rewards_proxy_go() {
-//     multiversx_sc_scenario::run_go("mandos/claim_rewards_proxy.scen.json");
+//     multiversx_sc_scenario::run_go("scenarios/claim_rewards_proxy.scen.json");
 // }
 
 // /*
 // #[test]
 // fn claim_rewards_proxy_after_mint_rewards_go() {
-//     multiversx_sc_scenario::run_go("mandos/claim_rewards_proxy_after_mint_rewards.scen.json");
+//     multiversx_sc_scenario::run_go("scenarios/claim_rewards_proxy_after_mint_rewards.scen.json");
 // }
 // */
 // #[test]
 // fn clear_unclaimable_assets_go() {
-//     multiversx_sc_scenario::run_go("mandos/clear_unclaimable_assets.scen.json");
+//     multiversx_sc_scenario::run_go("scenarios/clear_unclaimable_assets.scen.json");
 // }
 
 // #[test]
 // fn compound_mex_rewards_proxy_after_mint_rewards_go() {
-//     multiversx_sc_scenario::run_go("mandos/compound_mex_rewards_proxy_after_mint_rewards.scen.json");
+//     multiversx_sc_scenario::run_go("scenarios/compound_mex_rewards_proxy_after_mint_rewards.scen.json");
 // }
 
 // #[test]
 // fn enter_farm_proxy_go() {
-//     multiversx_sc_scenario::run_go("mandos/enter_farm_proxy.scen.json");
+//     multiversx_sc_scenario::run_go("scenarios/enter_farm_proxy.scen.json");
 // }
 
 // #[test]
 // fn enter_mex_farm_proxy_go() {
-//     multiversx_sc_scenario::run_go("mandos/enter_mex_farm_proxy.scen.json");
+//     multiversx_sc_scenario::run_go("scenarios/enter_mex_farm_proxy.scen.json");
 // }
 
 // #[test]
 // fn exit_farm_proxy_go() {
-//     multiversx_sc_scenario::run_go("mandos/exit_farm_proxy.scen.json");
+//     multiversx_sc_scenario::run_go("scenarios/exit_farm_proxy.scen.json");
 // }
 
 // #[test]
 // fn exit_mex_farm_proxy_after_compound_rewards_and_epoch_increase_go() {
 //     multiversx_sc_scenario::run_go(
-//         "mandos/exit_mex_farm_proxy_after_compound_rewards_and_epoch_increase.scen.json",
+//         "scenarios/exit_mex_farm_proxy_after_compound_rewards_and_epoch_increase.scen.json",
 //     );
 // }
 
 // #[test]
 // fn exit_mex_farm_proxy_after_compound_rewards_go() {
-//     multiversx_sc_scenario::run_go("mandos/exit_mex_farm_proxy_after_compound_rewards.scen.json");
+//     multiversx_sc_scenario::run_go("scenarios/exit_mex_farm_proxy_after_compound_rewards.scen.json");
 // }
 
 // /*
 // #[test]
 // fn exit_farm_proxy_after_mint_rewards_go() {
-//     multiversx_sc_scenario::run_go("mandos/exit_farm_proxy_after_mint_rewards.scen.json");
+//     multiversx_sc_scenario::run_go("scenarios/exit_farm_proxy_after_mint_rewards.scen.json");
 // }
 // */
 // #[test]
 // fn exit_mex_farm_proxy_after_mint_rewards_go() {
-//     multiversx_sc_scenario::run_go("mandos/exit_mex_farm_proxy_after_mint_rewards.scen.json");
+//     multiversx_sc_scenario::run_go("scenarios/exit_mex_farm_proxy_after_mint_rewards.scen.json");
 // }
 
 // #[test]
 // fn merge_locked_mex_tokens_go() {
-//     multiversx_sc_scenario::run_go("mandos/merge_locked_mex_tokens.scen.json");
+//     multiversx_sc_scenario::run_go("scenarios/merge_locked_mex_tokens.scen.json");
 // }
 
 // #[test]
 // fn multiple_claim_assets_go() {
-//     multiversx_sc_scenario::run_go("mandos/multiple_claim_assets.scen.json");
+//     multiversx_sc_scenario::run_go("scenarios/multiple_claim_assets.scen.json");
 // }
 
 // #[test]
 // fn remove_liquidity_proxy_go() {
-//     multiversx_sc_scenario::run_go("mandos/remove_liquidity_proxy.scen.json");
+//     multiversx_sc_scenario::run_go("scenarios/remove_liquidity_proxy.scen.json");
 // }
 
 // #[test]
 // fn remove_liquidity_proxy_after_swap_mex_go() {
-//     multiversx_sc_scenario::run_go("mandos/remove_liquidity_proxy_after_swap_mex.scen.json");
+//     multiversx_sc_scenario::run_go("scenarios/remove_liquidity_proxy_after_swap_mex.scen.json");
 // }
 
 // #[test]
 // fn remove_liquidity_proxy_after_swap_wegld_go() {
-//     multiversx_sc_scenario::run_go("mandos/remove_liquidity_proxy_after_swap_wegld.scen.json");
+//     multiversx_sc_scenario::run_go("scenarios/remove_liquidity_proxy_after_swap_wegld.scen.json");
 // }
 
 // #[test]
 // fn set_user_distribution_go() {
-//     multiversx_sc_scenario::run_go("mandos/set_user_distribution.scen.json");
+//     multiversx_sc_scenario::run_go("scenarios/set_user_distribution.scen.json");
 // }
 
 // #[test]
 // fn set_user_distribution_above_cap_go() {
-//     multiversx_sc_scenario::run_go("mandos/set_user_distribution_above_cap.scen.json");
+//     multiversx_sc_scenario::run_go("scenarios/set_user_distribution_above_cap.scen.json");
 // }
 
 // #[test]
 // fn set_user_distribution_duplicate_go() {
-//     multiversx_sc_scenario::run_go("mandos/set_user_distribution_duplicate.scen.json");
+//     multiversx_sc_scenario::run_go("scenarios/set_user_distribution_duplicate.scen.json");
 // }
 
 // #[test]
 // fn set_user_distribution_with_unlock_go() {
-//     multiversx_sc_scenario::run_go("mandos/set_user_distribution_with_unlock.scen.json");
+//     multiversx_sc_scenario::run_go("scenarios/set_user_distribution_with_unlock.scen.json");
 // }
 
 // #[test]
 // fn undo_last_community_distribution_go() {
-//     multiversx_sc_scenario::run_go("mandos/undo_last_community_distribution.scen.json");
+//     multiversx_sc_scenario::run_go("scenarios/undo_last_community_distribution.scen.json");
 // }
 
 // #[test]
 // fn undo_user_distribution_between_epochs_go() {
-//     multiversx_sc_scenario::run_go("mandos/undo_user_distribution_between_epochs.scen.json");
+//     multiversx_sc_scenario::run_go("scenarios/undo_user_distribution_between_epochs.scen.json");
 // }
 
 // #[test]
 // fn unlock_assets_basic_go() {
-//     multiversx_sc_scenario::run_go("mandos/unlock_assets_basic.scen.json");
+//     multiversx_sc_scenario::run_go("scenarios/unlock_assets_basic.scen.json");
 // }
