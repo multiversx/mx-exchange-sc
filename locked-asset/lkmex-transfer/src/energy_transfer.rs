ccb7ab834003491327bb28343471c8c3dbedd9ef
Sorin Petreasca <petreasca.sorin@gmail.com>
2023-02-17 11:42:34 +0200

    remove lkmex from locked token wrapper SC

--- locked-asset/lkmex-transfer/src/energy_transfer.rs
+++ locked-asset/lkmex-transfer/src/energy_transfer.rs
@@ -1,6 +1,6 @@
 multiversx_sc::imports!();
 
-use common_structs::{OldLockedTokenAttributes, PaymentsVec};
+use common_structs::PaymentsVec;
 use energy_factory::locked_token_transfer::ProxyTrait as _;
 use energy_query::Energy;
 use simple_lock::locked_token::LockedTokenAttributes;
@@ -37,30 +37,6 @@ pub trait EnergyTransferModule:
         self.set_energy_in_factory(from_user, energy);
     }
 
-    fn deduct_old_token_energy_from_sender(
-        &self,
-        from_user: ManagedAddress,
-        tokens: &PaymentsVec<Self::Api>,
-    ) {
-        if self.blockchain().is_smart_contract(&from_user) {
-            return;
-        }
-
-        let current_epoch = self.blockchain().get_block_epoch();
-        let mut energy = self.get_energy_entry(&from_user);
-
-        for token in tokens {
-            let attributes: OldLockedTokenAttributes<Self::Api> =
-                self.decode_legacy_token(&token.token_identifier, token.token_nonce);
-            let epoch_amount_pairs = attributes.get_unlock_amounts_per_epoch(&token.amount);
-            for pair in epoch_amount_pairs.pairs {
-                energy.update_after_unlock_any(&pair.amount, pair.epoch, current_epoch);
-            }
-        }
-
-        self.set_energy_in_factory(from_user, energy);
-    }
-
     fn add_energy_to_destination(&self, to_user: ManagedAddress, tokens: &PaymentsVec<Self::Api>) {
         let current_epoch = self.blockchain().get_block_epoch();
         let mut energy = self.get_energy_entry(&to_user);
@@ -77,29 +53,6 @@ pub trait EnergyTransferModule:
                 let epoch_diff = current_epoch - attributes.unlock_epoch;
                 let simulated_deplete_amount = &token.amount * epoch_diff;
                 energy.remove_energy_raw(BigUint::zero(), simulated_deplete_amount);
-            }
-        }
-
-        self.set_energy_in_factory(to_user, energy);
-    }
-
-    fn add_old_token_energy_to_destination(
-        &self,
-        to_user: ManagedAddress,
-        tokens: &PaymentsVec<Self::Api>,
-    ) {
-        if self.blockchain().is_smart_contract(&to_user) {
-            return;
-        }
-
-        let current_epoch = self.blockchain().get_block_epoch();
-        let mut energy = self.get_energy_entry(&to_user);
-
-        for token in tokens {
-            let attributes = self.decode_legacy_token(&token.token_identifier, token.token_nonce);
-            let epoch_amount_pairs = attributes.get_unlock_amounts_per_epoch(&token.amount);
-            for pair in epoch_amount_pairs.pairs {
-                energy.add_after_token_lock(&pair.amount, pair.epoch, current_epoch);
             }
         }
 
--- locked-asset/locked-token-wrapper/src/lib.rs
+++ locked-asset/locked-token-wrapper/src/lib.rs
@@ -15,22 +15,8 @@ pub trait LockedTokenWrapper:
     + legacy_token_decode_module::LegacyTokenDecodeModule
 {
     #[init]
-    fn init(
-        &self,
-        old_locked_token_id: TokenIdentifier,
-        new_locked_token_id: TokenIdentifier,
-        energy_factory_address: ManagedAddress,
-    ) {
-        self.require_valid_token_id(&old_locked_token_id);
-        self.require_valid_token_id(&new_locked_token_id);
+    fn init(&self, energy_factory_address: ManagedAddress) {
         self.require_sc_address(&energy_factory_address);
-
-        if self.old_locked_token().is_empty() {
-            self.old_locked_token().set_token_id(old_locked_token_id);
-        }
-        if self.locked_token().is_empty() {
-            self.locked_token().set_token_id(new_locked_token_id);
-        }
         self.energy_factory_address().set(&energy_factory_address);
     }
 
@@ -40,23 +26,15 @@ pub trait LockedTokenWrapper:
         let payment = self.call_value().single_esdt();
         let caller = self.blockchain().get_caller();
 
-        if payment.token_identifier == self.locked_token().get_token_id() {
-            self.deduct_energy_from_sender(
-                caller.clone(),
-                &ManagedVec::from_single_item(payment.clone()),
-            );
-
-            self.wrap_locked_token_and_send(&caller, payment)
-        } else if payment.token_identifier == self.old_locked_token().get_token_id() {
-            self.deduct_old_token_energy_from_sender(
-                caller.clone(),
-                &ManagedVec::from_single_item(payment.clone()),
-            );
-
-            self.wrap_locked_token_and_send(&caller, payment)
-        } else {
-            sc_panic!("Bad payment token");
-        }
+        require!(
+            payment.token_identifier == self.get_locked_token_id(),
+            "Bad payment tokens"
+        );
+        self.deduct_energy_from_sender(
+            caller.clone(),
+            &ManagedVec::from_single_item(payment.clone()),
+        );
+        self.wrap_locked_token_and_send(&caller, payment)
     }
 
     #[payable("*")]
@@ -71,14 +49,8 @@ pub trait LockedTokenWrapper:
         let payment = self.call_value().single_esdt();
         let original_locked_tokens = self.unwrap_locked_token(payment);
 
-        if original_locked_tokens.token_identifier == self.locked_token().get_token_id() {
+        if original_locked_tokens.token_identifier == self.get_locked_token_id() {
             self.add_energy_to_destination(
-                caller.clone(),
-                &ManagedVec::from_single_item(original_locked_tokens.clone()),
-            );
-        } else if original_locked_tokens.token_identifier == self.old_locked_token().get_token_id()
-        {
-            self.add_old_token_energy_to_destination(
                 caller.clone(),
                 &ManagedVec::from_single_item(original_locked_tokens.clone()),
             );
