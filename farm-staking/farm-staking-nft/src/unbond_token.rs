73020d043eb8c7a35c2ff9cba1008c2e0f6d58a0
Dorin Marian Iancu <dorin.iancu@multiversx.com>
2024-02-09 09:16:29 +0200

    fix token type

--- farm-staking/farm-staking-nft/src/unbond_token.rs
+++ farm-staking/farm-staking-nft/src/unbond_token.rs
@@ -8,17 +8,16 @@ pub trait UnbondTokenModule: permissions_module::PermissionsModule {
         &self,
         token_display_name: ManagedBuffer,
         token_ticker: ManagedBuffer,
-        num_decimals: usize,
     ) {
         self.require_caller_has_owner_or_admin_permissions();
 
         let payment_amount = self.call_value().egld_value().clone_value();
         self.unbond_token().issue_and_set_all_roles(
-            EsdtTokenType::Meta,
+            EsdtTokenType::NonFungible,
             payment_amount,
             token_display_name,
             token_ticker,
-            num_decimals,
+            0,
             None,
         );
     }
