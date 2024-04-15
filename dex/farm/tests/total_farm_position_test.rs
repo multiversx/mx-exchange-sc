6b5442e49fea971c2d372d3015b18b647b7bea99
Sorin Petreasca <petreasca.sorin@gmail.com>
2024-04-16 00:44:36 +0300

    total farm position through simple lock test

--- dex/farm/tests/total_farm_position_test.rs
+++ dex/farm/tests/total_farm_position_test.rs
@@ -8,9 +8,13 @@ use farm_setup::multi_user_farm_setup::{
     MultiUserFarmSetup, NonceAmountPair, BOOSTED_YIELDS_PERCENTAGE, MAX_PERCENTAGE,
     PER_BLOCK_REWARD_AMOUNT,
 };
-use multiversx_sc_scenario::{managed_address, managed_biguint, rust_biguint, DebugApi};
+use multiversx_sc::types::EsdtLocalRole;
+use multiversx_sc_scenario::{
+    managed_address, managed_biguint, managed_token_id, rust_biguint, DebugApi,
+};
+use sc_whitelist_module::SCWhitelistModule;
 
-use crate::farm_setup::multi_user_farm_setup::{FARM_TOKEN_ID, REWARD_TOKEN_ID};
+use crate::farm_setup::multi_user_farm_setup::{FARMING_TOKEN_ID, FARM_TOKEN_ID, REWARD_TOKEN_ID};
 
 #[test]
 fn total_farm_position_claim_test() {
@@ -935,4 +939,236 @@ fn total_farm_position_owner_change_test() {
     // Check users positions after merge
     farm_setup.check_user_total_farm_position(&first_user, farm_token_amount);
     farm_setup.check_user_total_farm_position(&second_user, farm_token_amount * 5);
+}
+
+#[test]
+fn total_farm_position_through_simple_lock_test() {
+    use multiversx_sc::storage::mappers::StorageTokenWrapper;
+    use simple_lock::locked_token::LockedTokenModule;
+    use simple_lock::proxy_farm::ProxyFarmModule;
+    use simple_lock::proxy_farm::*;
+    use simple_lock::proxy_lp::{LpProxyTokenAttributes, ProxyLpModule};
+    use simple_lock::SimpleLock;
+
+    const LOCKED_TOKEN_ID: &[u8] = b"NOOOO-123456";
+    const LOCKED_LP_TOKEN_ID: &[u8] = b"LKLP-123456";
+    const FARM_PROXY_TOKEN_ID: &[u8] = b"PROXY-123456";
+    const WEGLD_TOKEN_ID: &[u8] = b"WEGLD-abcdef";
+    const MEX_TOKEN_ID: &[u8] = b"MEX-abcdef"; // reward token ID
+
+    DebugApi::dummy();
+    let mut farm_setup = MultiUserFarmSetup::new(
+        farm::contract_obj,
+        energy_factory_mock::contract_obj,
+        energy_update::contract_obj,
+    );
+    let rust_zero = rust_biguint!(0);
+
+    // setup simple lock SC
+    let lock_wrapper = farm_setup.b_mock.create_sc_account(
+        &rust_zero,
+        Some(&farm_setup.owner),
+        simple_lock::contract_obj,
+        "Simple Lock Path",
+    );
+
+    let farm_addr = farm_setup.farm_wrapper.address_ref().clone();
+    farm_setup
+        .b_mock
+        .execute_tx(&farm_setup.owner, &lock_wrapper, &rust_zero, |sc| {
+            sc.init();
+            sc.locked_token()
+                .set_token_id(managed_token_id!(LOCKED_TOKEN_ID));
+            sc.lp_proxy_token()
+                .set_token_id(managed_token_id!(LOCKED_LP_TOKEN_ID));
+            sc.farm_proxy_token()
+                .set_token_id(managed_token_id!(FARM_PROXY_TOKEN_ID));
+            sc.add_farm_to_whitelist(
+                managed_address!(&farm_addr),
+                managed_token_id!(FARMING_TOKEN_ID),
+                FarmType::SimpleFarm,
+            );
+        })
+        .assert_ok();
+
+    // change farming token for farm + whitelist simple lock contract
+    farm_setup
+        .b_mock
+        .execute_tx(
+            &farm_setup.owner,
+            &farm_setup.farm_wrapper,
+            &rust_zero,
+            |sc| {
+                sc.farming_token_id()
+                    .set(&managed_token_id!(FARMING_TOKEN_ID));
+                sc.add_sc_address_to_whitelist(managed_address!(lock_wrapper.address_ref()));
+            },
+        )
+        .assert_ok();
+
+    farm_setup.b_mock.set_esdt_local_roles(
+        lock_wrapper.address_ref(),
+        LOCKED_LP_TOKEN_ID,
+        &[
+            EsdtLocalRole::NftCreate,
+            EsdtLocalRole::NftAddQuantity,
+            EsdtLocalRole::NftBurn,
+        ],
+    );
+    farm_setup.b_mock.set_esdt_local_roles(
+        lock_wrapper.address_ref(),
+        FARM_PROXY_TOKEN_ID,
+        &[
+            EsdtLocalRole::NftCreate,
+            EsdtLocalRole::NftAddQuantity,
+            EsdtLocalRole::NftBurn,
+        ],
+    );
+
+    // user lock tokens
+    let user_addr = farm_setup.first_user.clone();
+
+    let lp_proxy_token_attributes: LpProxyTokenAttributes<DebugApi> = LpProxyTokenAttributes {
+        lp_token_id: managed_token_id!(FARMING_TOKEN_ID),
+        first_token_id: managed_token_id!(WEGLD_TOKEN_ID),
+        first_token_locked_nonce: 1,
+        second_token_id: managed_token_id!(MEX_TOKEN_ID),
+        second_token_locked_nonce: 2,
+    };
+
+    farm_setup.b_mock.set_nft_balance(
+        &user_addr,
+        LOCKED_LP_TOKEN_ID,
+        1,
+        &rust_biguint!(1_000_000_000),
+        &lp_proxy_token_attributes,
+    );
+
+    farm_setup.b_mock.set_esdt_balance(
+        lock_wrapper.address_ref(),
+        FARMING_TOKEN_ID,
+        &rust_biguint!(1_000_000_000),
+    );
+
+    farm_setup.set_boosted_yields_rewards_percentage(BOOSTED_YIELDS_PERCENTAGE);
+    farm_setup.set_boosted_yields_factors();
+    farm_setup.b_mock.set_block_epoch(2);
+
+    let temp_user = farm_setup.third_user.clone();
+
+    farm_setup.check_user_total_farm_position(&user_addr, 0);
+
+    // first user enter farm twice (normal & through simple lock contract)
+    // enter farm through simple lock contract
+    let farm_in_amount = 50_000_000;
+    farm_setup.last_farm_token_nonce += 1;
+    farm_setup
+        .b_mock
+        .execute_esdt_transfer(
+            &user_addr,
+            &lock_wrapper,
+            LOCKED_LP_TOKEN_ID,
+            1,
+            &rust_biguint!(farm_in_amount),
+            |sc| {
+                let enter_farm_result = sc.enter_farm_locked_token(FarmType::SimpleFarm);
+                let (out_farm_token, _reward_token) = enter_farm_result.into_tuple();
+                assert_eq!(
+                    out_farm_token.token_identifier,
+                    managed_token_id!(FARM_PROXY_TOKEN_ID)
+                );
+                assert_eq!(out_farm_token.token_nonce, farm_setup.last_farm_token_nonce);
+                assert_eq!(out_farm_token.amount, managed_biguint!(farm_in_amount));
+            },
+        )
+        .assert_ok();
+
+    farm_setup.b_mock.check_nft_balance(
+        &user_addr,
+        FARM_PROXY_TOKEN_ID,
+        1,
+        &rust_biguint!(farm_in_amount),
+        Some(&FarmProxyTokenAttributes::<DebugApi> {
+            farm_type: FarmType::SimpleFarm,
+            farm_token_id: managed_token_id!(FARM_TOKEN_ID),
+            farm_token_nonce: 1,
+            farming_token_id: managed_token_id!(FARMING_TOKEN_ID),
+            farming_token_locked_nonce: 1,
+        }),
+    );
+
+    farm_setup.check_user_total_farm_position(&user_addr, farm_in_amount);
+
+    // enter farm directly
+    farm_setup.set_user_energy(&user_addr, 1_000, 2, 1);
+    farm_setup.enter_farm(&user_addr, farm_in_amount);
+
+    farm_setup.b_mock.check_nft_balance(
+        &user_addr,
+        FARM_TOKEN_ID,
+        farm_setup.last_farm_token_nonce,
+        &rust_biguint!(farm_in_amount),
+        Some(&FarmTokenAttributes::<DebugApi> {
+            reward_per_share: managed_biguint!(0),
+            compounded_reward: managed_biguint!(0),
+            entering_epoch: 2,
+            current_farm_amount: managed_biguint!(farm_in_amount),
+            original_owner: managed_address!(&user_addr),
+        }),
+    );
+
+    farm_setup.check_user_total_farm_position(&user_addr, farm_in_amount * 2);
+    farm_setup.check_farm_token_supply(farm_in_amount * 2);
+
+    // users claim rewards to get their energy registered
+    let _ = farm_setup.claim_rewards(&user_addr, 2, farm_in_amount);
+
+    // advance blocks - 10 blocks - 10 * 1_000 = 10_000 total rewards
+    // 7_500 base farm, 2_500 boosted yields
+    farm_setup.b_mock.set_block_nonce(10);
+
+    // random tx on end of week 1, to cummulate rewards
+    farm_setup.b_mock.set_block_epoch(6);
+    farm_setup.set_user_energy(&user_addr, 1_000, 6, 1);
+    farm_setup.set_user_energy(&temp_user, 1, 6, 1);
+    farm_setup.enter_farm(&temp_user, 1);
+    farm_setup.exit_farm(&temp_user, 4, 1);
+
+    // advance 1 week
+    farm_setup.b_mock.set_block_epoch(10);
+    farm_setup.set_user_energy(&user_addr, 1_000, 10, 1);
+
+    let total_farm_tokens = farm_in_amount * 2;
+
+    // first user claim with half total position
+    let first_base_farm_amt = farm_in_amount * 7_500 / total_farm_tokens;
+
+    // Boosted yields rewards formula
+    // total_boosted_rewards * (energy_const * user_energy / total_energy + farm_const * user_farm / total_farm) / (energy_const + farm_const)
+    // (total_boosted_rewards * energy_const * user_energy / total_energy + total_boosted_rewards * farm_const * user_farm / total_farm) / (energy_const + farm_const)
+    // (2_500 * 3 * 1_000 / 1_000 + 2_500 * 2 * 100_000_000 / 100_000_000) / (3 + 2)
+    // (7_500 + 2_500) / (5) = 2_500
+    let first_boosted_amt = 2_500; // 1000 energy & 100_000_000 farm tokens
+    let first_total_rewards = first_base_farm_amt + first_boosted_amt;
+
+    let first_received_reward_amt = farm_setup.claim_rewards(&user_addr, 3, farm_in_amount);
+
+    // Should be equal to half base generated rewards + full boosted generated rewards
+    assert_eq!(first_received_reward_amt, first_total_rewards);
+
+    farm_setup
+        .b_mock
+        .check_nft_balance::<FarmTokenAttributes<DebugApi>>(
+            &user_addr,
+            FARM_TOKEN_ID,
+            5,
+            &rust_biguint!(farm_in_amount),
+            None,
+        );
+
+    farm_setup.b_mock.check_esdt_balance(
+        &user_addr,
+        REWARD_TOKEN_ID,
+        &rust_biguint!(first_received_reward_amt),
+    );
 }
