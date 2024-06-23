c2a140af9a430a9d7fd63faf5818f7c123b65508
Sorin Petreasca <petreasca.sorin@gmail.com>
2024-06-24 02:33:10 +0300

    Farm Staking Proxy Legacy SC

---
+++ legacy-contracts/farm-staking-proxy-legacy/src/external_contracts_interactions.rs
@@ -0,0 +1,236 @@
+multiversx_sc::imports!();
+
+use common_structs::{RawResultWrapper, RawResultsType};
+
+use crate::result_types::*;
+
+mod farm_proxy {
+    multiversx_sc::imports!();
+
+    #[multiversx_sc::proxy]
+    pub trait FarmProxy {
+        #[payable("*")]
+        #[endpoint(exitFarm)]
+        fn exit_farm(&self) -> MultiValue2<EsdtTokenPayment, EsdtTokenPayment>;
+
+        #[view(getFarmingTokenId)]
+        fn farming_token_id(&self) -> TokenIdentifier;
+    }
+}
+
+mod farm_staking_proxy {
+    multiversx_sc::imports!();
+
+    #[multiversx_sc::proxy]
+    pub trait FarmStakingProxy {
+        #[payable("*")]
+        #[endpoint(unstakeFarmThroughProxy)]
+        fn unstake_farm_through_proxy(
+            &self,
+            original_caller: ManagedAddress,
+        ) -> MultiValue2<EsdtTokenPayment, EsdtTokenPayment>;
+    }
+}
+
+mod pair_proxy {
+    multiversx_sc::imports!();
+
+    #[multiversx_sc::proxy]
+    pub trait PairProxy {
+        #[payable("*")]
+        #[endpoint(removeLiquidity)]
+        fn remove_liquidity(
+            &self,
+            first_token_amount_min: BigUint,
+            second_token_amount_min: BigUint,
+        ) -> MultiValue2<EsdtTokenPayment, EsdtTokenPayment>;
+
+        #[endpoint(updateAndGetTokensForGivenPositionWithSafePrice)]
+        fn update_and_get_tokens_for_given_position_with_safe_price(
+            &self,
+            liquidity: BigUint,
+        ) -> MultiValue2<EsdtTokenPayment, EsdtTokenPayment>;
+    }
+}
+
+#[multiversx_sc::module]
+pub trait ExternalContractsInteractionsModule:
+    crate::lp_farm_token::LpFarmTokenModule + token_merge_helper::TokenMergeHelperModule
+{
+    // lp farm
+
+    fn lp_farm_exit(
+        &self,
+        lp_farm_token_nonce: u64,
+        lp_farm_token_amount: BigUint,
+    ) -> LpFarmExitResult<Self::Api> {
+        let lp_farm_token_id = self.lp_farm_token_id().get();
+        let lp_farm_address = self.lp_farm_address().get();
+        let raw_results: RawResultsType<Self::Api> = self
+            .lp_farm_proxy_obj(lp_farm_address)
+            .exit_farm()
+            .with_esdt_transfer((lp_farm_token_id, lp_farm_token_nonce, lp_farm_token_amount))
+            .execute_on_dest_context();
+
+        let mut results_wrapper = RawResultWrapper::new(raw_results);
+        results_wrapper.trim_results_front(2);
+
+        let mut lp_tokens: EsdtTokenPayment = results_wrapper.decode_next_result();
+        let mut lp_farm_rewards: EsdtTokenPayment = results_wrapper.decode_next_result();
+
+        let received_lp_token_identifier = lp_tokens.token_identifier.clone();
+        let lp_token_identifier = self.get_lp_farming_token_identifier();
+
+        if lp_token_identifier != received_lp_token_identifier {
+            core::mem::swap(&mut lp_tokens, &mut lp_farm_rewards);
+        }
+
+        LpFarmExitResult {
+            lp_tokens,
+            lp_farm_rewards,
+        }
+    }
+
+    fn get_lp_farming_token_identifier(&self) -> TokenIdentifier {
+        let lp_farm_address = self.lp_farm_address().get();
+        self.lp_farm_proxy_obj(lp_farm_address)
+            .farming_token_id()
+            .execute_on_dest_context()
+    }
+
+    // staking farm
+
+    fn staking_farm_unstake(
+        &self,
+        orig_caller: ManagedAddress,
+        staking_tokens: EsdtTokenPayment<Self::Api>,
+        farm_token_nonce: u64,
+        farm_token_amount: BigUint,
+    ) -> StakingFarmExitResult<Self::Api> {
+        let staking_farm_token_id = self.staking_farm_token_id().get();
+        let mut payments = ManagedVec::from_single_item(staking_tokens);
+        payments.push(EsdtTokenPayment::new(
+            staking_farm_token_id,
+            farm_token_nonce,
+            farm_token_amount,
+        ));
+
+        let staking_farm_address = self.staking_farm_address().get();
+        let raw_results: RawResultsType<Self::Api> = self
+            .staking_farm_proxy_obj(staking_farm_address)
+            .unstake_farm_through_proxy(orig_caller)
+            .with_multi_token_transfer(payments)
+            .execute_on_dest_context();
+
+        let mut results_wrapper = RawResultWrapper::new(raw_results);
+        results_wrapper.trim_results_front(2);
+
+        let unbond_staking_farm_token = results_wrapper.decode_next_result();
+        let staking_rewards = results_wrapper.decode_next_result();
+
+        StakingFarmExitResult {
+            unbond_staking_farm_token,
+            staking_rewards,
+        }
+    }
+
+    // pair
+
+    fn pair_remove_liquidity(
+        &self,
+        lp_tokens: EsdtTokenPayment<Self::Api>,
+        pair_first_token_min_amount: BigUint,
+        pair_second_token_min_amount: BigUint,
+    ) -> PairRemoveLiquidityResult<Self::Api> {
+        let pair_address = self.pair_address().get();
+        let raw_results: RawResultsType<Self::Api> = self
+            .pair_proxy_obj(pair_address)
+            .remove_liquidity(pair_first_token_min_amount, pair_second_token_min_amount)
+            .with_esdt_transfer((
+                lp_tokens.token_identifier,
+                lp_tokens.token_nonce,
+                lp_tokens.amount,
+            ))
+            .execute_on_dest_context();
+
+        let mut results_wrapper = RawResultWrapper::new(raw_results);
+        results_wrapper.trim_results_front(2);
+
+        let pair_first_token_payment: EsdtTokenPayment = results_wrapper.decode_next_result();
+        let pair_second_token_payment: EsdtTokenPayment = results_wrapper.decode_next_result();
+
+        let staking_token_id = self.staking_token_id().get();
+        let (staking_token_payment, other_token_payment) =
+            if pair_first_token_payment.token_identifier == staking_token_id {
+                (pair_first_token_payment, pair_second_token_payment)
+            } else if pair_second_token_payment.token_identifier == staking_token_id {
+                (pair_second_token_payment, pair_first_token_payment)
+            } else {
+                sc_panic!("Invalid payments received from Pair");
+            };
+
+        PairRemoveLiquidityResult {
+            staking_token_payment,
+            other_token_payment,
+        }
+    }
+
+    fn get_lp_tokens_safe_price(&self, lp_tokens_amount: BigUint) -> BigUint {
+        let pair_address = self.pair_address().get();
+        let raw_results: RawResultsType<Self::Api> = self
+            .pair_proxy_obj(pair_address)
+            .update_and_get_tokens_for_given_position_with_safe_price(lp_tokens_amount)
+            .execute_on_dest_context();
+
+        let mut results_wrapper = RawResultWrapper::new(raw_results);
+        results_wrapper.trim_results_front(2);
+
+        let first_token_info: EsdtTokenPayment = results_wrapper.decode_next_result();
+        let second_token_info: EsdtTokenPayment = results_wrapper.decode_next_result();
+
+        let staking_token_id = self.staking_token_id().get();
+        if first_token_info.token_identifier == staking_token_id {
+            first_token_info.amount
+        } else if second_token_info.token_identifier == staking_token_id {
+            second_token_info.amount
+        } else {
+            sc_panic!("Invalid Pair contract called");
+        }
+    }
+
+    // proxies
+
+    #[proxy]
+    fn staking_farm_proxy_obj(
+        &self,
+        sc_address: ManagedAddress,
+    ) -> farm_staking_proxy::Proxy<Self::Api>;
+
+    #[proxy]
+    fn lp_farm_proxy_obj(&self, sc_address: ManagedAddress) -> farm_proxy::Proxy<Self::Api>;
+
+    #[proxy]
+    fn pair_proxy_obj(&self, sc_address: ManagedAddress) -> pair_proxy::Proxy<Self::Api>;
+
+    // storage
+
+    #[view(getLpFarmAddress)]
+    #[storage_mapper("lpFarmAddress")]
+    fn lp_farm_address(&self) -> SingleValueMapper<ManagedAddress>;
+
+    #[view(getStakingFarmAddress)]
+    #[storage_mapper("stakingFarmAddress")]
+    fn staking_farm_address(&self) -> SingleValueMapper<ManagedAddress>;
+
+    #[view(getPairAddress)]
+    #[storage_mapper("pairAddress")]
+    fn pair_address(&self) -> SingleValueMapper<ManagedAddress>;
+
+    #[view(getStakingTokenId)]
+    #[storage_mapper("stakingTokenId")]
+    fn staking_token_id(&self) -> SingleValueMapper<TokenIdentifier>;
+
+    #[view(getFarmTokenId)]
+    #[storage_mapper("farmTokenId")]
+    fn staking_farm_token_id(&self) -> SingleValueMapper<TokenIdentifier>;
+}
---
+++ legacy-contracts/farm-staking-proxy-legacy/src/lib.rs
@@ -0,0 +1,91 @@
+#![no_std]
+
+multiversx_sc::imports!();
+
+pub mod dual_yield_token;
+pub mod external_contracts_interactions;
+pub mod lp_farm_token;
+pub mod result_types;
+
+pub type UnstakeResult<Api> = MultiValueEncoded<Api, EsdtTokenPayment<Api>>;
+
+#[multiversx_sc::contract]
+pub trait FarmStakingProxy:
+    dual_yield_token::DualYieldTokenModule
+    + external_contracts_interactions::ExternalContractsInteractionsModule
+    + lp_farm_token::LpFarmTokenModule
+    + token_merge_helper::TokenMergeHelperModule
+{
+    #[init]
+    fn init(&self) {}
+
+    #[payable("*")]
+    #[endpoint(unstakeFarmTokens)]
+    fn unstake_farm_tokens(
+        &self,
+        pair_first_token_min_amount: BigUint,
+        pair_second_token_min_amount: BigUint,
+    ) -> UnstakeResult<Self::Api> {
+        let caller = self.blockchain().get_caller();
+        let (payment_token, payment_nonce, payment_amount) =
+            self.call_value().single_esdt().into_tuple();
+        self.require_dual_yield_token(&payment_token);
+
+        let attributes = self.get_dual_yield_token_attributes(payment_nonce);
+        let lp_farm_token_amount =
+            self.get_lp_farm_token_amount_equivalent(&attributes, &payment_amount);
+        let lp_farm_exit_result =
+            self.lp_farm_exit(attributes.lp_farm_token_nonce, lp_farm_token_amount);
+
+        let remove_liq_result = self.pair_remove_liquidity(
+            lp_farm_exit_result.lp_tokens,
+            pair_first_token_min_amount,
+            pair_second_token_min_amount,
+        );
+
+        let staking_farm_token_amount =
+            self.get_staking_farm_token_amount_equivalent(&payment_amount);
+        let staking_farm_exit_result = self.staking_farm_unstake(
+            caller.clone(),
+            remove_liq_result.staking_token_payment,
+            attributes.staking_farm_token_nonce,
+            staking_farm_token_amount,
+        );
+        let unstake_result = self.send_unstake_payments(
+            &caller,
+            remove_liq_result.other_token_payment,
+            lp_farm_exit_result.lp_farm_rewards,
+            staking_farm_exit_result.staking_rewards,
+            staking_farm_exit_result.unbond_staking_farm_token,
+        );
+
+        self.burn_dual_yield_tokens(payment_nonce, &payment_amount);
+
+        unstake_result
+    }
+
+    fn send_unstake_payments(
+        &self,
+        caller: &ManagedAddress<Self::Api>,
+        other_token_payment: EsdtTokenPayment<Self::Api>,
+        lp_farm_rewards: EsdtTokenPayment<Self::Api>,
+        staking_rewards: EsdtTokenPayment<Self::Api>,
+        unbond_staking_farm_token: EsdtTokenPayment<Self::Api>,
+    ) -> UnstakeResult<Self::Api> {
+        let mut user_payments = ManagedVec::new();
+        if other_token_payment.amount > 0 {
+            user_payments.push(other_token_payment);
+        }
+        if lp_farm_rewards.amount > 0 {
+            user_payments.push(lp_farm_rewards);
+        }
+        if staking_rewards.amount > 0 {
+            user_payments.push(staking_rewards);
+        }
+        user_payments.push(unbond_staking_farm_token);
+
+        self.send().direct_multi(caller, &user_payments);
+
+        user_payments.into()
+    }
+}
