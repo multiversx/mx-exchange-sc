c0f89708c7610e15302c78830ef2f7cf4b9e1589
Sorin Petreasca <petreasca.sorin@gmail.com>
2025-11-13 16:28:22 +0200

    Remove duplicated checks in pair SC

--- dex/pair/src/pair_actions/swap.rs
+++ dex/pair/src/pair_actions/swap.rs
@@ -151,11 +151,6 @@ pub trait SwapModule:
         let caller = self.blockchain().get_caller();
         let output_payments = self.build_swap_output_payments(&swap_context);
 
-        require!(
-            output_payments.get(0).amount >= swap_context.output_token_amount,
-            ERROR_SLIPPAGE_EXCEEDED
-        );
-
         self.send_multiple_tokens_if_not_zero(&caller, &output_payments);
 
         self.emit_swap_event(&storage_cache, swap_context);
